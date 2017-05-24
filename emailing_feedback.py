6b91a7a2e9ccf50340f0b6c21247a6fc9c5e8d7d
Anton Akhmerov <anton.akhmerov@gmail.com>
2017-05-25 01:08:24 +0200

    more cleanup

--- emailing_feedback.py
+++ emailing_feedback.py
@@ -14,48 +14,11 @@ from email import encoders
 
 # db.use_db()
 
-default_template = jinja2.Template("""Dear {{student.first_name.split(' ') | first }} {{student.last_name}},
-
-Below please find attached the scans of your exam and our feedback.
-If you have any questions, don't hesitate to contant us during the exercise classes
-or in the course chat https://chat.quantumtinkerer.tudelft.nl/solidstate
-
-Additionally, you may want to compare your results with the overall performance
-of your colleagues over here:
-as well as a reference solution
-
-{% for problem in results | sort(attribute='name') if problem.feedback  -%}
-{{problem.name}} (your score: {{problem.score}} out of {{problem.max_score}}):
-{% for feedback in problem.feedback %}
-    * {{ (feedback.description or feedback.short) | wordwrap | indent(width=6) }}
-{% endfor %}
-{%- if problem.remarks %}
-    * {{ problem.remarks | wordwrap | indent(width=6) }}
-{% endif %}
-{% endfor %}
-
-{%- if student.total < 60 -%}
-Your grade is below 6.
-{%- elif student.total < 80 -%}
-Your grade is between 6 and 8.
-{%- else -%}
-Your grade is above 8.
-{%- endif%}
-
-Best,
-Solid state course team.
-""")
-
-default_template.trim_blocks = default_template.lstrip_blocks = True
-
 messages = []
-def form_email(exam_id, student_id, template=None, attach=True,
+def form_email(exam_id, student_id, template, attach=True,
                text_only=True, subject='Your results',
                email_from='no-reply@tudelft.nl'):
-    if template is None:
-        template = default_template
-    else:
-        template = jinja2.Template(template)
+    template = jinja2.Template(template)
 
     student, results, pages = db.solution_data(exam_id, student_id)
     text = template.render(student=student, results=results)

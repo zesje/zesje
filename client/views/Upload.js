cfed0155ab4a822b3f8522ed8a80ca15c94be8b2
Roosted7 <thomasroos@live.nl>
2018-01-15 15:13:03 +0100

    Add content to upload page

--- client/views/Upload.js
+++ client/views/Upload.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
 import Hero from '../components/Hero';
+import Footer from '../components/Footer';
 
 const Upload = () => {
   return (
@@ -10,8 +11,38 @@ const Upload = () => {
         
         <Hero title='Upload' subtitle='Omnomnomnom Exams!' />
 
-        <h1>React Router demo</h1>
-        Hoi dit de Upload
+        <section className="section">
+
+
+          <div className="container">
+            <h1 className='title'>Select your PDF file</h1>
+            <h5 className='subtitle'>And we will take care of the rest</h5>
+            
+            <hr />
+
+            <div className="file">
+              <label className="file-label">
+
+                <input className="file-input" type="file" name="resume" />
+                <span className="file-cta">
+                  <span className="file-icon">
+                    <i className="fa fa-upload"></i>
+                  </span>
+                  <span className="file-label">
+                    Choose a file…
+                  </span>
+                </span>
+
+              </label>
+            </div>
+
+
+          </div>
+
+        </section>
+
+        <Footer />
+
       </div>
   )
 }

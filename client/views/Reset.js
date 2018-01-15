e6211ee6edb01b10d2808814601e26af4669d5b4
Roosted7 <thomasroos@live.nl>
2018-01-15 16:22:24 +0100

    Fill the reset page

--- client/views/Reset.js
+++ client/views/Reset.js
@@ -11,10 +11,37 @@ const Reset = () => {
 
         <Hero title='Reset' subtitle="Please don't hurt my database :(" />
         
-        <h1>React Router demo</h1>
-        Hoi dit de Reset
-        <Footer />
-        
+        <section className="section">
+
+
+          <div className="container">
+            <h1 className='title'>Are your very sure?</h1>
+            <h5 className='subtitle'>This cannot be undone...</h5>
+            
+            <hr />
+
+            <div class="field is-grouped">
+              <div class="control">
+                <button class="button is-danger">Everything</button>
+              </div>
+              <div class="control">
+                <button class="button is-warning">People</button>
+              </div>
+              <div class="control">
+                <button class="button is-warning">Exam</button>
+              </div>
+              <div class="control">
+                <button class="button is-warning">Student validation</button>
+              </div>
+              <div class="control">
+                <button class="button is-warning">Gradeing</button>
+              </div>
+            </div>
+          </div>
+
+
+        </section>
+
       </div>
   )
 }

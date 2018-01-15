72b7515474ccf96584fc6d5bd07ee2f64a9558ad
Roosted7 <thomasroos@live.nl>
2018-01-15 15:48:36 +0100

    Fill the addGraders page

--- client/views/AddGraders.js
+++ client/views/AddGraders.js
@@ -11,10 +11,49 @@ const AddGraders = () => {
 
         <Hero title='Add Graders' subtitle='Many hands make light work' />
         
-        <h1>React Router demo</h1>
-        Hoi dit de AddGraders
+        <section className="section">
+
+
+          <div className="container">
+            <h1 className='title'>Enter the names</h1>
+            <h5 className='subtitle'>to add them to the system</h5>
+            
+            <hr />
+
+              
+            <div class="field has-addons">
+              <div class="select">
+                <select>
+                  <option>Henk de Vries</option>
+                  <option>Jan Janssen</option>
+                </select>
+              </div>
+              <div class="control">
+                <a class="button is-danger">Delete</a>
+              </div>
+            </div>
+
+            <div class="field has-addons">
+              <div class="control">
+                <input class="input" type="text" placeholder="Add someone" />
+              </div>
+              <div class="control">
+                <a class="button is-info">
+                  Add
+                </a>
+              </div>
+            </div>
+
+
+            
+
+
+          </div>
+
+        </section>
+
         <Footer />
-        
+
       </div>
   )
 }

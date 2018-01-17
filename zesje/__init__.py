4d42d05be7c7a628cfd6ab8b3b7022767d2198b0
Roosted7 <thomasroos@live.nl>
2018-01-17 01:01:41 +0100

    Laziest auth system working

--- zesje/__init__.py
+++ zesje/__init__.py
@@ -1,6 +1,7 @@
 from os import path
 from os.path import abspath, dirname, isfile
 from flask import Flask
+from flask_basicauth import BasicAuth
 
 from . import db, api
 
@@ -9,6 +10,14 @@ static_folder_path = path.join(abspath(dirname(__file__)), 'static')
 app = Flask(__name__,
             static_folder= static_folder_path)
 db.use_db()
+
+
+####### Very Very VERY import to use this outsite dev/sandbox environment
+app.config['BASIC_AUTH_USERNAME'] = 'test'
+app.config['BASIC_AUTH_PASSWORD'] = 'zesje'
+app.config['BASIC_AUTH_FORCE'] = False # Don't forget to set this to 'True'
+basic_auth = BasicAuth(app)
+
 
 app.register_blueprint(api.app, url_prefix='/api')
 

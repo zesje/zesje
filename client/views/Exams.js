5e0bab4b18e623bbd59fe7ae2754c976e4a4cf5e
Roosted7 <thomasroos@live.nl>
2018-01-21 14:50:50 +0100

    Use dropzone for drag and drop on exams page

--- client/views/Exams.js
+++ client/views/Exams.js
@@ -3,29 +3,34 @@ import NavBar from '../components/NavBar';
 import Hero from '../components/Hero';
 import Footer from '../components/Footer';
 
+import Dropzone from 'react-dropzone'
 
-var shortButton = {width: '336px'};
 
-const Upload = () => {
-  return (
-      <div>
+class Exams extends React.Component {
 
-        <NavBar />
+  onDrop(files) {
+    console.log(files);
+  }
+
+
+  render () {
+    return <div>
+
+      <NavBar />
         
-        <Hero title='Exams' subtitle="Omnomnomnom PDF's!" />
+      <Hero title='Exams' subtitle="Omnomnomnom PDF's!" />
 
-        <section className="section">
+      <section className="section">
 
+        <div className="container">
+          <div className="columns">
+            <div className="column has-text-centered">
+              <h3 className='title'>Upload new exam config</h3>
+              <h5 className='subtitle'>then we know that to do with PDF's</h5>
 
-          <div className="container">
-            <div className="columns">
-              <div className="column has-text-centered">
-                <h3 className='title'>Upload new exam config</h3>
-                <h5 className='subtitle'>then we know that to do with PDF's</h5>
-
+              <Dropzone style={{}} activeStyle={{borderStyle: 'dashed', width: 'fit-content', margin: 'auto'}} onDrop={this.onDrop.bind(this)}>
                 <div className="file has-name is-boxed is-centered">
-                  <label className="file-label">
-                    <input className="file-input" type="file" name="resume" />
+                  <label className="file-label"> 
                     <span className="file-cta">
                       <span className="file-icon">
                         <i className="fa fa-upload"></i>
@@ -39,29 +44,32 @@ const Upload = () => {
                     </span>
                   </label>
                 </div>
+              </Dropzone>
+
+              
+            </div>
+
+            <div className="column has-text-centered">
+              <h3 className='title'>And tweak the config</h3>
+              <h5 className='subtitle'>Fix misalignments</h5>
+              <div className="select">
+                <select>
+                  <option>Midterm 5-12</option>
+                  <option>Final 30-1</option>
+                </select>
               </div>
-
-              <div className="column has-text-centered">
-                <h3 className='title'>And tweak the config</h3>
-                <h5 className='subtitle'>Fix misalignments</h5>
-                <div className="select">
-                  <select>
-                    <option>Midterm 5-12</option>
-                    <option>Final 30-1</option>
-                  </select>
-                </div>
-                <textarea className="textarea" placeholder="YAML config will appear here..."></textarea>
-                <button className='button is-success'>Save</button>
-              </div>
+              <textarea className="textarea" placeholder="YAML config will appear here..."></textarea>
+              <button className='button is-success'>Save</button>
+            </div>
 
 
-              <div className="column has-text-centered">
-                <h3 className='title'>And upload PDF's</h3>
-                <h5 className='subtitle'>we will work some magic!</h5>
+            <div className="column has-text-centered">
+              <h3 className='title'>And upload PDF's</h3>
+              <h5 className='subtitle'>we will work some magic!</h5>
 
+              <Dropzone style={{}} activeStyle={{borderStyle: 'dashed', width: 'fit-content', margin: 'auto'}} onDrop={this.onDrop.bind(this)}>
                 <div className="file has-name is-boxed is-centered">
                   <label className="file-label">
-                    <input className="file-input" type="file" name="resume" />
                     <span className="file-cta">
                       <span className="file-icon">
                         <i className="fa fa-upload"></i>
@@ -75,30 +83,32 @@ const Upload = () => {
                     </span>
                   </label>
                 </div>
-
-                <br />
-
-                <aside className="menu">
-                <p className="menu-label">
-                  Previously uploaded
-                </p>
-                  <ul className="menu-list">
-                    <li>midterm.pdf</li>
-                    <li>final_exam.pdf</li>
-                  </ul>
-                </aside>
+              </Dropzone>
 
 
-              </div>
+              <br />
+
+              <aside className="menu">
+              <p className="menu-label">
+                Previously uploaded
+              </p>
+                <ul className="menu-list">
+                  <li>midterm.pdf</li>
+                  <li>final_exam.pdf</li>
+                </ul>
+              </aside>
+
+
             </div>
-
           </div>
-        </section>
 
-        <Footer />
+        </div>
+      </section>
 
-      </div>
-  )
+      <Footer />
+
+    </div>
+  }
 }
 
-export default Upload;
+export default Exams;

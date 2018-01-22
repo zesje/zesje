d830b02f4cade5ebb56f51e937da51da110338f5
Roosted7 <thomasroos@live.nl>
2018-01-22 19:33:47 +0100

    Remove surplus webpack module from config

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,4 @@
 const merge = require('webpack-merge');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {

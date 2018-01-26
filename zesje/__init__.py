1e93e263aca5feca76bfae142fa4431ea70e5ba9
Roosted7 <thomasroos@live.nl>
2018-01-26 17:22:32 +0100

    Use Flask_restful for better API handeling

--- zesje/__init__.py
+++ zesje/__init__.py
@@ -1,16 +1,16 @@
 from os import path
-from os.path import abspath, dirname, isfile
+from os.path import abspath, dirname
 
-from werkzeug.exceptions import NotFound
 from flask import Flask
 from flask_basicauth import BasicAuth
+from werkzeug.exceptions import NotFound
 
 from . import db, api
 
-static_folder_path = path.join(abspath(dirname(__file__)), 'static')
+STATIC_FOLDER_PATH = path.join(abspath(dirname(__file__)), 'static')
 
-app = Flask(__name__, static_folder=static_folder_path)
-db.use_db()
+app = Flask(__name__, static_folder=STATIC_FOLDER_PATH)
+app.register_blueprint(api.api_bp, url_prefix='/api')
 
 
 ####### Very Very VERY import to use this outsite dev/sandbox environment
@@ -20,11 +20,12 @@ app.config['BASIC_AUTH_FORCE'] = False # Don't forget to set this to 'True'
 basic_auth = BasicAuth(app)
 
 
-app.register_blueprint(api.app, url_prefix='/api')
+db.use_db()
 
 @app.route('/')
 @app.route('/<file>')
 def index(file='index.html'):
+    """Serve the static react content, otherwise fallback to the index.html"""
     try:
         return app.send_static_file(file)
     except NotFound:
---
+++ zesje/resources/graders.py
@@ -0,0 +1,59 @@
+from flask_restful import Resource, reqparse
+
+from .. import db
+
+parser = reqparse.RequestParser()
+parser.add_argument('first_name', type=str, required=True)
+parser.add_argument('last_name',  type=str, required=True)
+# TODO: when making new database structure, have only a single
+#       'name' field: it is just an identifier
+
+
+class Graders(Resource):
+    @db.session
+    def get(self):
+        """get all graders.
+
+
+        Returns
+        -------
+        list of:
+            id: int
+            first_name: str
+            last_name: str
+        """
+
+        return [
+                {
+                    'id' : g.id,
+                    'first_name' : g.first_name,
+                    'last_name' : g.last_name
+                }
+                for g in db.Grader.select()
+        ]
+
+    @db.session
+    def post(self):
+
+        args = parser.parse_args()
+
+        """add a grader.
+
+        Parameters
+        ----------
+        first_name: str
+        last_name: str
+
+        Returns
+        -------
+        id: int
+        first_name: str
+        last_name: str
+        """
+
+        db.Grader(first_name= args['first_name'],
+                    last_name=args['last_name'])
+        db.orm.commit()
+
+        return {
+        }

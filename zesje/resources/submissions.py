0630d81089091d9816cc18eecd95940eb249a9a6
Joseph Weston <joseph@weston.cloud>
2018-03-09 10:41:06 +0100

    add PUT method to submissions endpoint for assigning students

--- zesje/resources/submissions.py
+++ zesje/resources/submissions.py
@@ -1,7 +1,7 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from pony import orm
 
-from ..models import Exam, Submission
+from ..models import Exam, Submission, Student
 
 class Submissions(Resource):
     """Getting a list of submissions, and assigning students to them."""
@@ -49,3 +49,49 @@ class Submissions(Resource):
             for s in Submission.select(lambda s: s.exam == exam)
                                .order_by(Submission.copy_number)
         ]
+
+    put_parser = reqparse.RequestParser()
+    put_parser.add_argument('studentID', type=int, required=True)
+
+    @orm.db_session
+    def put(self, exam_id, submission_id=None):
+        """Assign a student to the given submission.
+
+        Expects a json payload in the format::
+
+            {"studentID": 1234567}
+
+
+        Parameters
+        ----------
+        exam_id : int
+        submission_id : int
+            The copy number of the submission. This uniquely identifies
+            the submission *within a given exam*.
+
+        """
+        # have to allow 'submission_id' to be optional in the signature
+        # because otherwise we just 500 if it's not provided.
+        if submission_id is None:
+            msg = "Submission ID must be provided when assigning student"
+            return dict(status=400, message=msg), 400
+
+        args = self.put_parser.parse_args()
+
+        exam = Exam[exam_id]
+        submission = Submission.get(exam=exam, copy_number=submission_id)
+        if not submission:
+            raise orm.core.ObjectNotFound(Submission)
+
+        student = Student.get(id=args.studentID)
+        if not student:
+            msg = f'Student {args.studentID} does not exist'
+            return dict(status=404, message=msg), 404
+
+        submission.student = student
+        submission.signature_validated = True
+        return  {
+                'id': submission.copy_number,
+                'studentID': submission.student.id,
+                'validated': ssubmission.signature_validated,
+            }

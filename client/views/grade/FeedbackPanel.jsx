1edeeb49bbc261abfd2cbe16a324dcba441ddb5d
Roosted7 <thomasroos@live.nl>
2018-04-19 20:01:16 +0200

    Basics of feedbackpanel, changes not pushed to sevr yet

--- client/views/grade/FeedbackPanel.jsx
+++ client/views/grade/FeedbackPanel.jsx
@@ -7,46 +7,7 @@ import FeedbackBlock from './FeedbackBlock.jsx';
 class FeedbackPanel extends React.Component {
 
     state = {
-        feedback: [],
-        remark: null,
-        remarkActive: false
-    }
-
-    addRemark = () => {
-        this.setState({
-            remarkActive: true
-        })
-    }
-
-    componentDidMount = () => {
-        if (this.props.problem) {
-            api.get('feedback/' + this.props.problem)
-                .then(feedback => {
-                    this.setState({
-                        feedback: feedback
-                    })
-                })
-        }
-    }
-
-    componentWillReceiveProps = (nextProps) => {
-        if (this.props.problem !== nextProps.problem) {
-            api.get('feedback/' + nextProps.problem)
-                .then(feedback => {
-                    this.setState({
-                        feedback: feedback
-                    })
-                })
-        }
-    }
-
-    shouldComponentUpdate = (nextProps, nextState) => {
-        if (this.props.problem !== nextProps.problem || this.state.feedback != nextState.feedback || this.state.rem) {
-            return true;            
-        } else {
-            console.log('halting re-render')
-            return true;
-        }
+        remark: null
     }
 
     render() {
@@ -56,8 +17,8 @@ class FeedbackPanel extends React.Component {
                 <p className="panel-heading">
                     Feedback
                 </p>
-                {this.state.feedback.map((feedback, i) =>
-                    <FeedbackBlock key={i} index={i} feedback={feedback} checked={false} onClick={this.props.editFeedback} />
+                {this.props.problem.feedback.map((feedback, i) =>
+                    <FeedbackBlock key={feedback.id} feedback={feedback} checked={false} onClick={this.props.editFeedback} />
                 )}
                 {this.state.remarkActive ?
                     <div className="panel-block">

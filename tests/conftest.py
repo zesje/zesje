2ff8765316593944fd3147881dda1f746108be84
Jamy Mahabier <jamysanjay@gmail.com>
2018-05-15 22:24:37 +0200

    Add pytest to project
    Add some plumbing for pytest

---
+++ tests/conftest.py
@@ -0,0 +1,9 @@
+import os
+
+import pytest
+
+
+# Adapted from https://stackoverflow.com/a/46062148/1062698
+@pytest.fixture
+def datadir():
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')

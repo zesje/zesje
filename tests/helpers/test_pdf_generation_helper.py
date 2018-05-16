84315355dce079c9b303005c538347b5669c007d
Jamy Mahabier <jamysanjay@gmail.com>
2018-05-16 17:37:23 +0200

    Add PDF generation helper and its tests

---
+++ tests/helpers/test_pdf_generation_helper.py
@@ -0,0 +1,133 @@
+import os
+from io import BytesIO
+
+import PIL
+import pytest
+from ssim import compute_ssim
+from reportlab.lib.pagesizes import A4
+from reportlab.lib.units import mm
+from reportlab.pdfgen.canvas import Canvas as RLCanvas
+from wand.color import Color as WandColor
+from wand.image import Image as WandImage
+
+from zesje.helpers import pdf_generation_helper
+
+
+# Mock fixtures #
+
+
+@pytest.fixture
+def mock_generate_datamatrix(monkeypatch, datadir):
+    def mock_return(exam_id, page_num, copy_num):
+        return PIL.Image.open(os.path.join(datadir, 'datamatrix.png'))
+    monkeypatch.setattr(pdf_generation_helper, 'generate_datamatrix',
+                        mock_return)
+
+
+@pytest.fixture
+def mock_generate_id_grid(monkeypatch):
+    def mock_return(canv, x, y):
+        canv.setFont('Helvetica', 14)
+        canv.drawString(x * mm, y * mm, 'Beautiful ID Grid')
+    monkeypatch.setattr(pdf_generation_helper, 'generate_id_grid', mock_return)
+
+
+# Utility methods #
+
+
+# Method to check whether the pages in a PDF are equal to a list of images.
+def assert_pdf_and_images_are_equal(pdf_filename, images,
+                                    ssim_threshold=0.998):
+    images_pdf = []
+    with WandImage(filename=pdf_filename, resolution=150) as pdf:
+        for i, page in enumerate(pdf.sequence):
+            with WandImage(page) as img:
+                img.background_color = WandColor('white')
+                img.alpha_channel = 'remove'
+                blob = img.make_blob(format='bmp')
+                pil_image = PIL.Image.open(BytesIO(blob)).convert(mode='RGB')
+                images_pdf.append(pil_image)
+
+    images_input = [x.convert(mode='RGB') for x in images]
+
+    assert len(images_pdf) == len(images_input)
+
+    # For some reason, the images never match exactly, there seems to be a
+    # subtle (not visible to the human eye) difference. Therefore, we use the
+    # similarity metric SSIM to compare images.
+    # For some examples of SSIM, see the section 'Demonstration' at
+    # http://www.cns.nyu.edu/~lcv/ssim/
+    for i in range(len(images_pdf)):
+        assert compute_ssim(images_pdf[i], images_input[i]) >= ssim_threshold
+
+
+# Tests #
+
+
+def test_generate_overlay(mock_generate_datamatrix, mock_generate_id_grid,
+                          datadir, tmpdir):
+    filename = os.path.join(str(tmpdir), 'file.pdf')
+
+    canv = RLCanvas(filename, pagesize=A4)
+    pdf_generation_helper._generate_overlay(canv, 'ABCDEFGHIJKL', 1, 2, 25,
+                                            270, 150, 270)
+    canv.save()
+
+    img_filenames = [os.path.join(datadir, f'overlay{i}.png') for i in [0, 1]]
+    images = [PIL.Image.open(x) for x in img_filenames]
+    assert_pdf_and_images_are_equal(filename, images)
+
+
+def test_generate_pdfs_num_files(datadir, tmpdir):
+    blank_pdf = os.path.join(datadir, 'blank-2pages.pdf')
+
+    num_copies = 3
+
+    pdf_generation_helper.generate_pdfs(blank_pdf, 'ABCDEFGHIJKL', str(tmpdir),
+                                        num_copies, 25, 270, 150, 270)
+
+    assert len(tmpdir.listdir()) == num_copies
+
+
+def test_generate_pdfs_blank(mock_generate_datamatrix, mock_generate_id_grid,
+                             datadir, tmpdir):
+    blank_pdf = os.path.join(datadir, 'blank-2pages.pdf')
+
+    pdf_generation_helper.generate_pdfs(blank_pdf, 'ABCDEFGHIJKL', str(tmpdir),
+                                        2, 25, 270, 150, 270)
+
+    img_filenames = [os.path.join(datadir, f'overlay{i}.png') for i in [0, 1]]
+    images = [PIL.Image.open(x) for x in img_filenames]
+    filenames = [os.path.join(tmpdir, x) for x in ['00000.pdf', '00001.pdf']]
+    assert_pdf_and_images_are_equal(filenames[0], images)
+    assert_pdf_and_images_are_equal(filenames[1], images)
+
+
+def test_join_pdfs(mock_generate_datamatrix, mock_generate_id_grid,
+                   datadir, tmpdir):
+    directory = os.path.join(datadir, 'test-join-pdfs')
+    out = os.path.join(tmpdir, 'out.pdf')
+    num_copies = 2
+
+    pdf_generation_helper.join_pdfs(directory, out, num_copies)
+
+    image_filename = os.path.join(datadir, 'blank.png')
+    images = [PIL.Image.open(image_filename)] * 4
+    assert_pdf_and_images_are_equal(out, images)
+
+
+# Untested:
+#
+# - generate_datamatrix()
+#   Given a single data string, there may be many generated DataMatrix codes
+#   that correspond to that string. Therefore, a test to check the generated
+#   DataMatrix code would break if we ever move to a different encoding library
+#   or if the current library ever implements a more efficient encoding
+#   algorithm. A test for this function would therefore be brittle.
+#   Furthermore, the function is very short, so the need for testing is not
+#   very high.
+#
+# - generate_id_grid()
+#   This method is trivial. It just draws some stuff using ReportLab. If
+#   ReportLab works correctly, this method will certainly also work correctly,
+#   so there's no need to test it.
---
+++ zesje/helpers/pdf_generation_helper.py
@@ -0,0 +1,134 @@
+import os
+from io import BytesIO
+from tempfile import NamedTemporaryFile
+
+import PIL
+from pdfrw import PdfReader, PdfWriter, PageMerge
+from pystrich.datamatrix import DataMatrixEncoder
+from reportlab.lib.pagesizes import A4
+from reportlab.lib.units import mm
+from reportlab.pdfgen import canvas
+
+
+_output_pdf_filename_format = '{0:05d}.pdf'
+
+
+def generate_pdfs(exam_pdf_file, exam_id, output_dir, num_copies, id_grid_x,
+                  id_grid_y, datamatrix_x, datamatrix_y):
+    """
+    Generate the final PDFs from the original exam PDF.
+    
+    Parameters
+    ----------
+    TODO
+    """
+    exam_pdf = PdfReader(exam_pdf_file)
+    
+    for copy_idx in range(num_copies):
+        # ReportLab can't deal with file handles, but only with file names,
+        # so we have to use a named file
+        with NamedTemporaryFile() as overlay_file:
+            # Generate overlay
+            overlay_canv = canvas.Canvas(overlay_file.name, pagesize=A4)
+            _generate_overlay(overlay_canv, exam_id, copy_idx,
+                              len(exam_pdf.pages), id_grid_x, id_grid_y,
+                              datamatrix_x, datamatrix_y)
+            overlay_canv.save()
+            
+            # Merge overlay and exam
+            overlay_pdf = PdfReader(overlay_file)
+            for page_idx, exam_page in enumerate(exam_pdf.pages):
+                # First prepare the exam merge, and then add it to the overlay.
+                # If it were the other way around, all the overlays would
+                # 'stick' to the exam.
+                exam_merge = PageMerge().add(exam_page)[0]
+                overlay_merge = PageMerge(overlay_pdf.pages[page_idx]) \
+                    .add(exam_merge)
+                overlay_merge.render()
+            path = os.path.join(output_dir,
+                                _output_pdf_filename_format.format(copy_idx))
+            PdfWriter(path, trailer=overlay_pdf).write()
+
+
+def join_pdfs(directory, output_file, num_copies):
+    """
+    Join all the final PDFs into a single big PDF.
+    
+    Parameters
+    ----------
+    TODO
+    """
+    writer = PdfWriter()
+    
+    for copy_idx in range(num_copies):
+        path = os.path.join(directory,
+                            _output_pdf_filename_format.format(copy_idx))
+        writer.addpages(PdfReader(path).pages)
+    
+    writer.write(output_file)
+
+
+def generate_id_grid(canv, x, y):
+    """
+    Generates the student ID grid on the given canvas at the given coordinates.
+
+    Parameters
+    ----------
+    TODO
+    """
+    canv.setFont('Helvetica', 14)
+    canv.drawString(x * mm, y * mm, 'Beautiful ID Grid')
+
+
+def generate_datamatrix(exam_id, page_num, copy_num):
+    """
+    Generates a DataMatrix code to be used on a page.
+    
+    To maintain a consistent size of the DataMatrix codes, adhere to (# of
+    letters in exam ID) + 2 * (# of digits in exam ID) = C for a certain
+    constant C. The reason for this is that pyStrich encodes two digits in as
+    much space as one letter.
+    
+    If maximum interchangeability with version 1 QR codes is desired (error
+    correction level M), use exam IDs composed of only uppercase letters, and
+    composed of at most 12 letters.
+
+    Parameters
+    ----------
+    exam_id : str
+        The identifier of the exam
+    page_num : int
+        The page number
+    copy_num : int
+        The number of the copy
+
+    Returns
+    -------
+    PIL.Image.Image object
+        The PIL image of the DataMatrix code, including quiet zone (you don't
+        need to add a quiet zone yourself)
+    """
+    
+    data = f'{exam_id}/{copy_num:04d}/{page_num:02d}'
+    
+    image_bytes = DataMatrixEncoder(data).get_imagedata(cellsize=2)
+    return PIL.Image.open(BytesIO(image_bytes))
+
+
+def _generate_overlay(canv, exam_id, copy_num, num_pages, id_grid_x, id_grid_y,
+                      datamatrix_x, datamatrix_y):
+    """
+    Generates an overlay ('watermark') PDF, which can then be overlaid onto
+    the exam PDF.
+
+    Parameters
+    ----------
+    TODO
+    """
+    # ID grid on first page only
+    generate_id_grid(canv, id_grid_x, id_grid_y)
+
+    for page_num in range(num_pages):
+        datamatrix = generate_datamatrix(exam_id, page_num, copy_num)
+        canv.drawInlineImage(datamatrix, datamatrix_x * mm, datamatrix_y * mm)
+        canv.showPage()

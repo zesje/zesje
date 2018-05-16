04fde6442d73c5686104997342d60c1170546421
Nick Cleintuar <n.a.cleintuar@student.tudelft.nl>
2018-05-16 15:57:42 +0200

    Added test for calc_angle

---
+++ tests/test_rotation_scan.py
@@ -0,0 +1,15 @@
+import pytest
+import cv2
+import math
+from zesje.helpers import image_helper
+
+@pytest.mark.parametrize("test_input1, test_input2, expected", [
+    (cv2.KeyPoint(0,100,0), cv2.KeyPoint(0,1000,0), 0),
+    (cv2.KeyPoint(25,25,0), cv2.KeyPoint(50,50,0), -45),
+    (cv2.KeyPoint(25,25,0), cv2.KeyPoint(50,0,0), 45)
+])
+def test_calc_angle(test_input1, test_input2, expected):
+    assert math.isclose(image_helper.calc_angle(test_input1, test_input2), expected, abs_tol=0.1)
+
+def test_yolo():
+    assert 1==1

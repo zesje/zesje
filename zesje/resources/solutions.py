ae27db5dc947a991bcfd729b281163b3eeae5eeb
Roosted7 <thomasroos@live.nl>
2018-05-25 17:40:09 +0200

    Add remark POST api endpoint

--- zesje/resources/solutions.py
+++ zesje/resources/solutions.py
@@ -6,6 +6,7 @@ from pony import orm
 
 from ..models import Exam, Submission, Problem, Solution, FeedbackOption
 
+
 class Solutions(Resource):
     """ Solution provided on a specific problem and exam """
 
@@ -34,14 +35,50 @@ class Solutions(Resource):
             raise orm.core.ObjectNotFound(Solution)
 
         return {
-                    'feedback': [fb.id for fb in solution.feedback],
-                    'gradedBy': solution.graded_by,
-                    'gradedAt': solution.graded_at.isoformat() if solution.graded_at else None,
-                    'remarks': solution.remarks
-                }
+            'feedback': [fb.id for fb in solution.feedback],
+            'gradedBy': solution.graded_by,
+            'gradedAt': solution.graded_at.isoformat() if solution.graded_at else None,
+            'remarks': solution.remarks
+        }
+
+    post_parser = reqparse.RequestParser()
+    post_parser.add_argument('remark', type=str, required=True)
+
+    @orm.db_session
+    def post(self, exam_id, submission_id, problem_id):
+        """Toggles an existing feedback option
+
+        Parameters
+        ----------
+            remark: string
+
+        Returns
+        -------
+            true (if succesfull)
+        """
+
+        problem = Problem[problem_id]
+        exam = Exam[exam_id]
+
+        sub = Submission.get(exam=exam, copy_number=submission_id)
+        if not sub:
+            raise orm.core.ObjectNotFound(Submission)
+
+        solution = Solution.get(submission=sub, problem=problem)
+        if not solution:
+            raise orm.core.ObjectNotFound(Solution)
+
+        args = self.post_parser.parse_args()
+
+        solution.graded_at = datetime.now()
+
+        solution.remarks = args.remark
+
+        return True
 
     put_parser = reqparse.RequestParser()
     put_parser.add_argument('id', type=int, required=True)
+
     @orm.db_session
     def put(self, exam_id, submission_id, problem_id):
         """Toggles an existing feedback option
@@ -66,10 +103,9 @@ class Solutions(Resource):
         if not solution:
             raise orm.core.ObjectNotFound(Solution)
 
-
         args = self.put_parser.parse_args()
 
-        fb = FeedbackOption.get(id = args.id)
+        fb = FeedbackOption.get(id=args.id)
         if not fb:
             raise orm.core.ObjectNotFound(FeedbackOption)
 
@@ -77,8 +113,7 @@ class Solutions(Resource):
 
         if fb in solution.feedback:
             solution.feedback.remove(fb)
-            return { 'state': False }
+            return {'state': False}
         else:
             solution.feedback.add(fb)
-            return { 'state': True }
-
+            return {'state': True}

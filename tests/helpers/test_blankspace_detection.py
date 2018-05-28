6995f162460a7e863d88d133622ac4f79e8e83ef
Nick Cleintuar <N.A.Cleintuar@student.tudelft.nl>
2018-05-28 13:17:44 +0200

    Updated mocks

--- tests/helpers/test_blankspace_detection.py
+++ tests/helpers/test_blankspace_detection.py
@@ -21,6 +21,14 @@ def mock_check_space_idwidget_true(monkeypatch):
                         mock_return)
 
 
+@pytest.fixture
+def mock_check_space_corner_true(monkeypatch):
+    def mock_return(bin_im):
+        return True
+    monkeypatch.setattr(image_helper, 'check_space_corner',
+                        mock_return)
+
+
 # Tests
 
 @pytest.mark.parametrize('name',
@@ -32,7 +40,10 @@ def mock_check_space_idwidget_true(monkeypatch):
                                                      'data',
                                                      'source_exams',
                                                      'compatible')))
-def test_detect_space_corner_compatible(name, datadir):
+def test_detect_space_corner_compatible(name, datadir,
+                                        mock_check_space_datamatrix_true,
+                                        mock_check_space_idwidget_true):
+
     pdf_path = os.path.join(datadir, 'source_exams', 'compatible', f'{name}')
     result = image_helper.check_enough_blankspace(pdf_path)
 
@@ -49,7 +60,10 @@ def test_detect_space_corner_compatible(name, datadir):
                                                      'data',
                                                      'source_exams',
                                                      'incompatible')))
-def test_detect_space_corner_incompatible(name, datadir):
+def test_detect_space_corner_incompatible(name, datadir,
+                                          mock_check_space_datamatrix_true,
+                                          mock_check_space_idwidget_true):
+
     pdf_path = os.path.join(datadir, 'source_exams', 'incompatible', f'{name}')
     result = image_helper.check_enough_blankspace(pdf_path)
 

aa94e256eac9e1338260f3550968d8ab719e1b27
Nick Cleintuar <N.A.Cleintuar@student.tudelft.nl>
2018-06-01 16:41:38 +0200

    Fixed linter warnings

--- zesje/helpers/scan_helper.py
+++ zesje/helpers/scan_helper.py
@@ -388,7 +388,7 @@ def get_student_number(image_path, widget):
     mask = np.zeros((h+2, w+2), np.uint8)
 
     # Floodfill from point (0, 0)
-    cv2.floodFill(im_floodfill, mask, (0,0), 255)
+    cv2.floodFill(im_floodfill, mask, (0, 0), 255)
 
     # Invert floodfilled image
     im_floodfill_inv = cv2.bitwise_not(im_floodfill)

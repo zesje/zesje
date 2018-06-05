7bda67d22633b23acff00a62c15c82eefc6fbb3a
Nick Cleintuar <N.A.Cleintuar@student.tudelft.nl>
2018-06-06 00:20:51 +0200

    Added extra tests

--- tests/helpers/test_check_keypoints.py
+++ tests/helpers/test_check_keypoints.py
@@ -8,10 +8,15 @@ from zesje.helpers import image_helper
 # Tests whether RuntimeError are raised correctly
 
 
-@pytest.mark.parametrize('keypoints_input, expected_error', [
-    ([(0, 0), (750, 750)], True)],
-    ids=['Not enough corner markers'])
 def test_runtime_error_check_corner_marker(keypoints_input, expected_error):
+@pytest.mark.parametrize('keypoints_input, error_expected', [
+    ([(0, 0), (750, 750)], True),
+    ([(0, 0), (750, 0), (0, 750), (750, 750), (250, 250)], True),
+    ([(0, 0), (750, 0), (250, 250)], True),
+    ([(0, 0), (750, 0), (750, 750)], False)],
+    ids=['Not enough corner markers', 'Too many corner',
+         'Two markers detected in same corner',
+         'Enough markers, evenly spread'])
 
     image_data = np.zeros((1000, 1000))
     if expected_error:

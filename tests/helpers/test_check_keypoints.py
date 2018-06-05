9987523ad38459e7e262a07b1b342313b7fab9a6
Nick Cleintuar <N.A.Cleintuar@student.tudelft.nl>
2018-06-06 00:21:08 +0200

    Changed naming for clarity

--- tests/helpers/test_check_keypoints.py
+++ tests/helpers/test_check_keypoints.py
@@ -8,7 +8,6 @@ from zesje.helpers import image_helper
 # Tests whether RuntimeError are raised correctly
 
 
-def test_runtime_error_check_corner_marker(keypoints_input, expected_error):
 @pytest.mark.parametrize('keypoints_input, error_expected', [
     ([(0, 0), (750, 750)], True),
     ([(0, 0), (750, 0), (0, 750), (750, 750), (250, 250)], True),
@@ -17,9 +16,10 @@ def test_runtime_error_check_corner_marker(keypoints_input, expected_error):
     ids=['Not enough corner markers', 'Too many corner',
          'Two markers detected in same corner',
          'Enough markers, evenly spread'])
+def test_runtime_error_check_corner_marker(keypoints_input, error_expected):
 
     image_data = np.zeros((1000, 1000))
-    if expected_error:
+    if error_expected:
         with pytest.raises(RuntimeError):
             image_helper.check_corner_keypoints(image_data, keypoints_input)
     else:

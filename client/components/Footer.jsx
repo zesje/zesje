184582e1f76f5625565c41e36fa737cd67fce9c9
Roosted7 <thomasroos@live.nl>
2018-06-24 23:29:11 +0200

    Add AGPL license to the Footer

--- client/components/Footer.jsx
+++ client/components/Footer.jsx
@@ -7,7 +7,7 @@ const Hero = (props) => {
                 <div className="content has-text-centered">
                 <p>
                     <strong>Zesje</strong> by <a href="https://gitlab.kwant-project.org/zesje/zesje/graphs/react" target="_blank">the team</a>.
-                    The source code is licensed <a href="https://spdx.org/licenses/BSD-3-Clause.html" target="_blank"> BSD 3 </a>
+                    The source code is licensed <a href="https://choosealicense.com/licenses/agpl-3.0/" target="_blank"> AGPLv3 </a>
                     and available <a href="https://gitlab.kwant-project.org/zesje/zesje/tree/react" target="_blank"> here!</a>
                 </p>
                 </div>

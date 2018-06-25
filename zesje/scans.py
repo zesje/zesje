a53f4f4dfdc760bb332361779f1bfeead6b66b3b
Anton Akhmerov <anton.akhmerov@gmail.com>
2018-06-26 00:25:22 +0200

    eliminate a larger part of the image for blob detection

--- zesje/scans.py
+++ zesje/scans.py
@@ -545,8 +545,8 @@ def find_corner_marker_keypoints(image_array):
     bin_im = ~(bin_im_inv | im_floodfill_inv)
 
     # Filter out everything in the center of the image
-    bin_im[round(0.125 * h):round(0.875 * h),
-           round(0.125 * w):round(0.875 * w)] = 255
+    bin_im[round(0.125 * h):round(0.875 * h)] = 255
+    bin_im[:, round(0.125 * w):round(0.875 * w)] = 255
 
     # Detect objects which look like corner markers
     params = cv2.SimpleBlobDetector_Params()

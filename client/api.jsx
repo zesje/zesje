b8557f614c4a8fbd2ab067081581dd825cff3c43
Roosted7 <thomasroos@live.nl>
2018-06-30 14:04:42 +0200

    Fix standard issues in api.jsx

--- client/api.jsx
+++ client/api.jsx
@@ -1,25 +1,26 @@
 // Javascript does not have built-in functionality to do proper typechecking
 // https://stackoverflow.com/questions/7893776/the-most-accurate-way-to-check-js-objects-type
+
 function _typeof (a) {
   return Object.prototype.toString.call(a).slice(8, -1)
 }
 
 function _fetch (method) {
   return (endpoint, data) => {
-    var headers = new Headers()
-    if (_typeof(data) == 'Object') {
+    var headers = new window.Headers()
+    if (_typeof(data) === 'Object') {
       headers.append('Content-Type', 'application/json')
       data = JSON.stringify(data)
     }
 
-    return fetch('/api/' + endpoint, {
+    return window.fetch('/api/' + endpoint, {
       method: method,
       credentials: 'same-origin',
       body: data,
       headers: headers
     })
       .catch(error =>
-        console.error('Error in', method, endpoint, 'with data', data))
+        console.error('Error: ', error, ' in', method, endpoint, 'with data', data))
       .then(resp => {
         if (!resp.ok) {
           throw resp

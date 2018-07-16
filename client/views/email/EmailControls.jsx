99e03c95dacc4a04aa682cbb58f74f6131be22da
Anton Akhmerov <anton.akhmerov@gmail.com>
2018-07-16 21:03:05 +0200

    be more exact in a confirmation request

--- client/views/email/EmailControls.jsx
+++ client/views/email/EmailControls.jsx
@@ -77,7 +77,7 @@ const SendWithConfirmationButton = (props) => (
       'button is-primary is-fullwidth ' +
       (props.sending ? 'is-loading' : null)
     }
-    confirmationText={'You are about to email all the students on the course'}
+    confirmationText={'Email all students who took this exam?'}
     contentText={'This may take some time.'}
     onConfirm={props.onSend}
     disabled={props.disabled}

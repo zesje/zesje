57a3750c1b46eb29e014663f4cefd75c7807336b
Pim Otte <otte.pim@gmail.com>
2018-12-29 12:44:04 +0100

    Correct regex to match the actual cache

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ module.exports = {
   'moduleNameMapper': {
     '\\.(css|md)$': 'identity-obj-proxy'
   },
-  'testPathIgnorePatterns': ['/node_modules/', '/.yarn_cache/'],
+  'testPathIgnorePatterns': ['/node_modules/', '/.yarn-cache/'],
   'setupTestFrameworkScriptFile': '<rootDir>testSetup.js'
 }

ea7aee69f183c254feb3fb1e6be662dd7161b820
Pim Otte <otte.pim@gmail.com>
2018-12-29 12:33:46 +0100

    Ignoring yarn cache for tests

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   'moduleNameMapper': {
     '\\.(css|md)$': 'identity-obj-proxy'
   },
+  'testPathIgnorePatterns': ['/node_modules/', '/.yarn_cache/'],
   'setupTestFrameworkScriptFile': '<rootDir>testSetup.js'
 }

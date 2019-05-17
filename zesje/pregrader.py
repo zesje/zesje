995e9038ea5f5d41a8d84daebea26852ea135aea
Ruben Young On <r.d.youngon@student.tudelft.nl>
2019-05-17 12:40:06 +0200

    Added pregrader instructions

---
+++ zesje/pregrader.py
@@ -0,0 +1,11 @@
+
+from zesje.images import guess_dpi
+
+import zesje.database
+
+# 1 - get image
+# 2 - get database coordinates of all mc_options
+# 3 - resolution / dpi
+# 4 - get corner marker keypoints
+# 5 - determine blank pdf corner markers vs submission corner markers
+# 6 - transform submission image

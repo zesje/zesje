70c90407689e914a38924b714055927e48c4480e
Ruben Young On <r.d.youngon@student.tudelft.nl>
2019-05-21 16:51:10 +0200

    Added function to add feedback to solution

--- zesje/pregrader.py
+++ zesje/pregrader.py
@@ -1,8 +1,3 @@
-
-from zesje.images import guess_dpi
-
-import zesje.database
-
 # 3 - resolution / dpi to convert points to inches to pixels
 # 4 - get corner marker keypoints per page
 # 5 - optional? determine blank pdf corner markers vs submission corner markers
@@ -14,9 +9,10 @@ import zesje.database
 
 # coupled feedback cannot be deleted
 
-from zesje.database import Exam
+from zesje.database import db, Exam, FeedbackOption
 
 from zesje.images import guess_dpi
+
 
 def pregrade(exam_token, image):
     # get image
@@ -30,6 +26,20 @@ def pregrade(exam_token, image):
     coords = [(cb.x, cb.y) for cb in mc_options]
 
     pass
+
+
+def add_feedback_to_solution(solution):
+    problem = solution.problem
+
+    for mc_option in problem.mc_options:
+        box = (mc_option.x, mc_option.y)
+
+        # check width and so forth
+
+        # if box is filled
+        if True:
+            solution.feedback = mc_option.feedback
+            db.session.commit()
 
 
 def _locate_checkbox():

26f773a72f588e4b1d263916c9ed3770d65f972b
Ruben Young On <r.d.youngon@student.tudelft.nl>
2019-05-21 15:54:49 +0200

    Added start of pre grading

--- zesje/pregrader.py
+++ zesje/pregrader.py
@@ -3,8 +3,6 @@ from zesje.images import guess_dpi
 
 import zesje.database
 
-# 1 - get image
-# 2 - get database coordinates of all mc_options in exam
 # 3 - resolution / dpi to convert points to inches to pixels
 # 4 - get corner marker keypoints per page
 # 5 - optional? determine blank pdf corner markers vs submission corner markers
@@ -16,9 +14,20 @@ import zesje.database
 
 # coupled feedback cannot be deleted
 
+from zesje.database import Exam
 
-def pregrade(exam_id):
-    # Get exam pages
+from zesje.images import guess_dpi
+
+def pregrade(exam_token, image):
+    # get image
+    image = None
+
+    exam = Exam.query.get(exam_token=exam_token)
+
+    problems = exam.problems
+    mc_options = [problem.mc_options for problem in problems]
+
+    coords = [(cb.x, cb.y) for cb in mc_options]
 
     pass
 

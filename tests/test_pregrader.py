d615f08ff8327eb3079b148146a8752de6a65d2d
Robin Bijl <r.a.bijl@student.tudelft.nl>
2019-05-27 16:09:33 +0200

    adds setup and tests to test ideal box is filled circumstances

--- tests/test_pregrader.py
+++ tests/test_pregrader.py
@@ -20,11 +20,11 @@ def scanned_image(datadir):
 @pytest.fixture
 def scanned_image_keypoints(scanned_image):
     corner_markers = scans.find_corner_marker_keypoints(scanned_image)
-    top_left_point, fixed_corner_keypoints = images.fix_corner_markers(corner_markers, scanned_image.shape)
-    return top_left_point
+    fixed_corner_keypoints = images.fix_corner_markers(corner_markers, scanned_image.shape)
+    return fixed_corner_keypoints
 
 
 @pytest.mark.parametrize('box_coords, result', [((346, 479), True), ((370, 479), False), ((393, 479), True),
                                                 ((416, 479), True), ((439, 479), True), ((155, 562), True)],)
 def test_ideal_crops(datadir, box_coords, result, scanned_image_keypoints, scanned_image):
-    assert pregrader.box_is_filled(box_coords, scanned_image, scanned_image_keypoints) == result
+    assert pregrader.box_is_filled(box_coords, scanned_image, scanned_image_keypoints[0]) == result

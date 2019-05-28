5435318937dcaa76c6a6d559078a97c059dc089f
Robin Bijl <r.a.bijl@student.tudelft.nl>
2019-05-28 14:05:49 +0200

    adds tests for cases where words are underneath

--- tests/test_pregrader.py
+++ tests/test_pregrader.py
@@ -25,8 +25,10 @@ def scanned_image_keypoints(scanned_image):
 
 
 @pytest.mark.parametrize('box_coords, result', [((346, 479), True), ((370, 479), False), ((393, 479), True),
-                                                ((416, 479), True), ((439, 479), True), ((155, 562), True)],)
-def test_ideal_crops(datadir, box_coords, result, scanned_image_keypoints, scanned_image):
+                                                ((416, 479), True), ((439, 479), True), ((155, 562), True)],
+                         ids=["1 filled", "2 empty", "3 marked with line", "4 completely filled",
+                              "5 marked with an x", "e marked with a cirle inside"])
+def test_ideal_crops(box_coords, result, scanned_image_keypoints, scanned_image):
     assert pregrader.box_is_filled(box_coords, scanned_image, scanned_image_keypoints[0]) == result
 
 
@@ -35,5 +37,13 @@ def test_ideal_crops(datadir, box_coords, result, scanned_image_keypoints, scann
                          ids=["1 filled bottom right", "1 filled top left", "5 filled with a bit of 6",
                               "4 fully filled with the label", "6 empty with label",
                               "7 partially  cropped, filled and a part of 6"])
-def test_shifted_crops(datadir, box_coords, result, scanned_image_keypoints, scanned_image):
+def test_shifted_crops(box_coords, result, scanned_image_keypoints, scanned_image):
+    assert pregrader.box_is_filled(box_coords, scanned_image, scanned_image_keypoints[0]) == result
+
+
+@pytest.mark.parametrize('box_coords, result', [((60, 562), True), ((107, 562), True),
+                                                ((131, 562), False)],
+                         ids=["A filled with trailing letter", "C filled with letters close",
+                              "D blank with trailing letter"])
+def test_trailing_text(box_coords, result, scanned_image_keypoints, scanned_image):
     assert pregrader.box_is_filled(box_coords, scanned_image, scanned_image_keypoints[0]) == result

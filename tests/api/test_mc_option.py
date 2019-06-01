be4d3d4e1c6c6b2f482460ff77d0324c041429d9
Ruben Young On <r.d.youngon@student.tudelft.nl>
2019-06-01 16:38:56 +0200

    Tests are now split among different files

--- tests/api/test_mc_option.py
+++ tests/api/test_mc_option.py
@@ -1,64 +1,35 @@
 import pytest
 
 from flask import Flask, json
-from zesje.api import api_bp
+
 from zesje.database import db, Exam, Problem, ProblemWidget
 
 
-def add_test_data():
-    exam1 = Exam(id=1, name='exam 1', finalized=False)
-    exam2 = Exam(id=2, name='exam 2', finalized=True)
-    exam3 = Exam(id=3, name='exam 3', finalized=False)
-
-    db.session.add(exam1)
-    db.session.add(exam2)
-    db.session.add(exam3)
-    db.session.commit()
-
-    problem1 = Problem(id=1, name='Problem 1', exam_id=1)
-    problem2 = Problem(id=2, name='Problem 2', exam_id=2)
-    problem3 = Problem(id=3, name='Problem 3', exam_id=3)
-
-    db.session.add(problem1)
-    db.session.add(problem2)
-    db.session.add(problem3)
-    db.session.commit()
-
-    problem_widget_1 = ProblemWidget(id=1, name='problem widget', problem_id=1, page=2,
-                                     width=100, height=150, x=40, y=200, type='problem_widget')
-    db.session.add(problem_widget_1)
-    db.session.commit()
-
-
-@pytest.fixture(scope="module")
-def app():
-    app = Flask(__name__, static_folder=None)
-
-    app.config.update(
-        SQLALCHEMY_DATABASE_URI='sqlite:///:memory:',
-        SQLALCHEMY_TRACK_MODIFICATIONS=False  # Suppress future deprecation warning
-    )
-    db.init_app(app)
-
-    with app.app_context():
-        db.drop_all()
-        db.create_all()
-
-    app.register_blueprint(api_bp, url_prefix='/api')
-
-    return app
-
-
 @pytest.fixture
-def test_client(app):
-    client = app.test_client()
-
-    yield client
-
+def add_test_data(app):
     with app.app_context():
-        db.drop_all()
-        db.create_all()
-        add_test_data()
+        exam1 = Exam(id=1, name='exam 1', finalized=False)
+        exam2 = Exam(id=2, name='exam 2', finalized=True)
+        exam3 = Exam(id=3, name='exam 3', finalized=False)
+
+        db.session.add(exam1)
+        db.session.add(exam2)
+        db.session.add(exam3)
+        db.session.commit()
+
+        problem1 = Problem(id=1, name='Problem 1', exam_id=1)
+        problem2 = Problem(id=2, name='Problem 2', exam_id=2)
+        problem3 = Problem(id=3, name='Problem 3', exam_id=3)
+
+        db.session.add(problem1)
+        db.session.add(problem2)
+        db.session.add(problem3)
+        db.session.commit()
+
+        problem_widget_1 = ProblemWidget(id=1, name='problem widget', problem_id=1, page=2,
+                                         width=100, height=150, x=40, y=200, type='problem_widget')
+        db.session.add(problem_widget_1)
+        db.session.commit()
 
 
 def mco_json():
@@ -84,7 +55,7 @@ def test_not_present(test_client):
     assert data['message'] == "Multiple choice question with id 1 does not exist."
 
 
-def test_add(test_client):
+def test_add(test_client, add_test_data):
     req = mco_json()
     response = test_client.put('/api/mult-choice/', data=req)
 
@@ -97,7 +68,7 @@ def test_add(test_client):
     assert data['status'] == 200
 
 
-def test_add_get(test_client):
+def test_add_get(test_client, add_test_data):
     req = mco_json()
 
     response = test_client.put('/api/mult-choice/', data=req)
@@ -143,7 +114,7 @@ def test_update_put(test_client):
     assert data['message'] == f'Multiple choice question with id {id} updated'
 
 
-def test_delete(test_client):
+def test_delete(test_client, add_test_data):
     req = mco_json()
 
     response = test_client.put('/api/mult-choice/', data=req)
@@ -165,7 +136,7 @@ def test_delete_not_present(test_client):
     assert data['message'] == f'Multiple choice question with id {id} does not exist.'
 
 
-def test_delete_finalized_exam(test_client):
+def test_delete_finalized_exam(test_client, add_test_data):
     mc_option_json = {
         'x': 100,
         'y': 40,
@@ -190,28 +161,3 @@ EXAM API TESTS
 '''
 
 
-def test_get_exams(test_client):
-    mc_option_1 = {
-        'x': 100,
-        'y': 40,
-        'problem_id': 1,
-        'page': 1,
-        'label': 'a',
-        'name': 'test'
-    }
-    test_client.put('/api/mult-choice/', data=mc_option_1)
-
-    mc_option_2 = {
-        'x': 100,
-        'y': 40,
-        'problem_id': 1,
-        'page': 1,
-        'label': 'a',
-        'name': 'test'
-    }
-    test_client.put('/api/mult-choice/', data=mc_option_2)
-
-    response = test_client.get('/api/exams/1')
-    data = json.loads(response.data)
-
-    assert len(data['problems'][0]['mc_options']) == 2

1979bc0dfd6b546e71ab0aa5fb684820dfb6c00e
Ruben Young On <r.d.youngon@student.tudelft.nl>
2019-06-12 18:48:57 +0200

    Changed widget to MultipleChoiceOption

--- tests/api/test_widgets.py
+++ tests/api/test_widgets.py
@@ -2,7 +2,7 @@ import pytest
 
 from flask import json
 
-from zesje.database import db, Exam, Problem, ProblemWidget
+from zesje.database import db, Exam, Problem, FeedbackOption, MultipleChoiceOption, ProblemWidget
 
 
 @pytest.fixture
@@ -17,7 +17,14 @@ def add_test_data(app):
         problem_widget_1 = ProblemWidget(id=1, name='problem widget', problem_id=1, page=2,
                                          width=100, height=150, x=40, y=200, type='problem_widget')
         db.session.add(problem_widget_1)
+        db.session.commit()
 
+        feedback_option = FeedbackOption(id=1, problem_id=1, text='text', description='desc', score=1)
+        db.session.add(feedback_option)
+        db.session.commit()
+
+        mc_option = MultipleChoiceOption(id=2, label='a', feedback_id=1, x=10, y=30, name='mco', type='mcq_widget')
+        db.session.add(mc_option)
         db.session.commit()
 
 
@@ -26,11 +33,11 @@ ACTUAL TESTS
 '''
 
 
-def test_delete_finalized_exam(test_client, add_test_data):
+def test_update_mco_finalized_exam(test_client, add_test_data):
     """
     Attempt to update a ProblemWidget in a finalized exam
     """
-    widget_id = 1
+    widget_id = 2
 
     req_body = {'x': 50}
 
--- zesje/api/widgets.py
+++ zesje/api/widgets.py
@@ -1,7 +1,7 @@
 from flask_restful import Resource
 from flask import request
 
-from ..database import db, Widget, ExamWidget, ProblemWidget
+from ..database import db, Widget, ExamWidget, MultipleChoiceOption
 
 
 class Widgets(Resource):
@@ -15,7 +15,7 @@ class Widgets(Resource):
             return dict(status=404, message=msg), 404
         elif isinstance(widget, ExamWidget) and widget.exam.finalized:
             return dict(status=403, message=f'Exam is finalized'), 403
-        elif isinstance(widget, ProblemWidget) and widget.problem.exam.finalized:
+        elif isinstance(widget, MultipleChoiceOption) and widget.feedback.problem.exam.finalized:
             return dict(status=403, message=f'Exam is finalized'), 403
 
         # will 400 on malformed json

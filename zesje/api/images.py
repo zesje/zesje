42af61405bfe04e3d77cc8e295558724cd179639
Robin Bijl <r.a.bijl@student.tudelft.nl>
2019-06-12 14:07:33 +0200

    makes the size of the highlighted box dependant an variable and dpi

--- zesje/api/images.py
+++ zesje/api/images.py
@@ -6,6 +6,8 @@ import cv2
 from ..images import get_box, guess_dpi
 from ..database import Exam, Submission, Problem, Page, Solution
 
+BOX_SIZE = 9
+
 
 def get(exam_id, problem_id, submission_id, full_page=False):
     """get image for the given problem.
@@ -68,8 +70,9 @@ def get(exam_id, problem_id, submission_id, full_page=False):
             if option.feedback_id in fb:
                 x = int((option.x) / 72 * dpi)
                 y = int((option.y) / 72 * dpi)
-                x1 = x + 20
-                y1 = y + 20
+                box_length = int(BOX_SIZE / 72 * dpi)
+                x1 = x + box_length
+                y1 = y + box_length
                 page_im = cv2.rectangle(page_im, (x, y), (x1, y1), (0, 255, 0), 3)
 
     if not full_page:

84d02e22f32be51936c467fbad4a192a1daca599
Hugo Kerstens <hugokk@hotmail.nl>
2019-10-18 13:23:57 +0200

    Move tooltip component to own file

---
+++ client/components/Tooltip.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Tooltip = (props) => {
+  if (!props.text) {
+    return null
+  }
+
+  let tooltipClass = 'icon tooltip is-tooltip-right '
+  if (props.text.length > 45) {
+    tooltipClass += 'is-tooltip-multiline '
+  }
+
+  return (
+    <span
+      className={tooltipClass}
+      data-tooltip={props.text}
+    >
+      <i className='fa fa-comment' />
+    </span>
+  )
+}
+
+export default Tooltip

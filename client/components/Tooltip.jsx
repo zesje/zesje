d243aa1c7698dd67394d0eb92a7ebc069f0f42ed
Hugo Kerstens <hugokk@hotmail.nl>
2019-10-18 14:16:55 +0200

    Extend tooltip functionality

--- client/components/Tooltip.jsx
+++ client/components/Tooltip.jsx
@@ -1,21 +1,28 @@
 import React from 'react'
 
+import 'bulma-tooltip/dist/css/bulma-tooltip.min.css'
+
 const Tooltip = (props) => {
   if (!props.text) {
     return null
   }
 
-  let tooltipClass = 'icon tooltip is-tooltip-right '
+  let tooltipIcon = props.icon || 'comment'
+
+  let tooltipLocation = props.location || 'right'
+
+  let tooltipClass = 'icon tooltip is-tooltip-' + tooltipLocation
   if (props.text.length > 45) {
-    tooltipClass += 'is-tooltip-multiline '
+    tooltipClass += ' is-tooltip-multiline'
   }
 
   return (
     <span
       className={tooltipClass}
       data-tooltip={props.text}
+      onClick={props.clickAction}
     >
-      <i className='fa fa-comment' />
+      <i className={'fa fa-' + tooltipIcon} />
     </span>
   )
 }

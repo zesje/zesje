58c94a6b2155838ee50805458e6ce54c1ccdae46
Otto Kaaij <o.k.n.kaaij@student.tudelft.nl>
2019-10-26 13:37:51 +0200

    Add generation of exam pdf

--- scripts/example_data.py
+++ scripts/example_data.py
@@ -6,6 +6,8 @@ from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
 from tempfile import NamedTemporaryFile
 
+import lorem
+
 sys.path.append(os.getcwd())
 import zesje  # noqa: E402
 
@@ -24,10 +26,30 @@ app.register_blueprint(zesje.api.api_bp, url_prefix='/api')
 
 client = app.test_client()
 
-for _ in range(args.exams):
 
+def generate_exam_page(pdf, page_num):
+    for problem_num in range(3):
+        generate_problem(pdf, 75, 600 - (170 * problem_num), 3 * page_num + problem_num + 1)
+    pdf.showPage()
+
+
+def generate_problem(pdf, x, y, problem_num):
+    margin = 5
+    box_size = {'w': 450, 'h': 100}
+    question = lorem.sentence().replace('.', '?')
+    pdf.drawString(x, y, f'{problem_num}: {question}')
+    pdf.rect(x, y - box_size['h'] - margin, box_size['w'], box_size['h'])
+
+
+def generate_exam_pdf():
     with NamedTemporaryFile() as pdf_file:
-        canvas = canvas.Canvas(pdf_file.name, pagesize=A4)
-        for _ in range(args.pages - 1):
-            canvas.showPage()
-        canvas.save()
+        pdf = canvas.Canvas(pdf_file.name, pagesize=A4)
+        for i in range(args.pages):
+            generate_exam_page(pdf, page_num=i)
+        pdf.save()
+
+
+for _ in range(args.exams):
+    generate_exam_pdf()
+
+

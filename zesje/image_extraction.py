021055b7d39fc27155a53552db628635906b2512
Max van Deursen <M.vanDeursen@student.tudelft.nl>
2019-11-07 16:07:21 +0100

    Partially rewrite image_extraction.py
    In this commit, there are some internal changes to image_extraction.py.
    These changes are:
    - Change function arguments of both PikePDF and Wand extraction
    functions to use the pikepdf.Page instance instead of the page number
    and the pikepdf.Pdf instance. In turn, the for-loop is changed to
    instead loop over the enumeration of the index and pages of the
    pikepdf.Page instance
    - The extract_image_pikepdf now uses page.images to retrieve images from
    the page. This slightly changes the behaviour of this function, because
    it does no longer depend on the XObject existing. Because of this, the
    AttributeError is now only thrown if the MediaBox attribute does not
    exist. The docstring is also changed to reflect this change, as well as
    the unit test which evaluates this behaviour.
    - The extract_image_pikepdf now uses the actual difference in aspect
    ratio between the PDF page and the image, rather than the difference in
    ratio between width and height.
    - Add the description of the dpi argument of the extract_image_wand
    function to its docstring.
    - Use the allowed line length of 120 characters to the full extent.

--- zesje/image_extraction.py
+++ zesje/image_extraction.py
@@ -10,55 +10,43 @@ from wand.image import Image as WandImage
 def extract_images(filename, dpi=300):
     """Yield all images from a PDF file.
 
-    Tries to use PikePDF to extract the images from the given PDF.
-    If PikePDF is not able to extract the image from a page,
-    it continues to use Wand to flatten the rest of the pages.
+    Tries to use PikePDF to extract the images from the given PDF. If PikePDF is not able to extract the image from a
+    page, it continues to use Wand to flatten the rest of the pages.
     """
-
     with Pdf.open(filename) as pdf_reader:
         use_wand = False
 
-        total = len(pdf_reader.pages)
-
-        for pagenr in range(total):
+        for index, page in enumerate(pdf_reader.pages):
             if not use_wand:
                 try:
                     # Try to use PikePDF, but catch any error it raises
-                    img = extract_image_pikepdf(pagenr, pdf_reader)
+                    img = extract_image_pikepdf(page)
 
-                except Exception:
+                except (ValueError, AttributeError):
                     # Fallback to Wand if extracting with PikePDF failed
                     use_wand = True
 
             if use_wand:
-                img = extract_image_wand(pagenr, pdf_reader, dpi)
+                img = extract_image_wand(page, dpi)
 
             if img.mode == 'L':
                 img = img.convert('RGB')
 
-            yield img, pagenr+1
+            yield img, index + 1
 
 
-def extract_image_pikepdf(pagenr, reader):
-    """Extracts an image as an array from the designated page
+def extract_image_pikepdf(page):
+    """Extracts an image as a PIL Image from the designated page.
 
-    This method uses PikePDF to extract the image and only works
-    when there is a single image present on the page with the
-    same aspect ratio as the page.
-
-    We do not check for the actual size of the image on the page,
-    since this size depends on the draw instruction rather than
-    the embedded image object available to pikepdf.
-
-    Raises an error if not exactly image is present or the image
-    does not have the same aspect ratio as the page.
+    This method uses PikePDF to extract the image. It works on the assumption that the scan is included as a single
+    embedded image within the page. This means that the PDF should include a single embedded image which has the same
+    aspect ratio of the complete page. If there is not a single image embedded on the page, or if this image does not
+    share the same aspect ratio to the page, a ValueError is thrown.
 
     Parameters
     ----------
-    pagenr : int
-        Page number to extract
-    reader : pikepdf.Pdf instance
-        The pdf reader to read the page from
+    page: pikepdf.Page
+        Page from which to extract the image
 
     Returns
     -------
@@ -68,56 +56,47 @@ def extract_image_pikepdf(pagenr, reader):
     Raises
     ------
     ValueError
-        if not exactly one image is found on the page or the image
-        does not have the same aspect ratio as the page
+        if not exactly one image is found on the page or the image does not have the same aspect ratio as the page
     AttributeError
-        if no XObject or MediaBox is present on the page
+        if the MediaBox of a page is not defined
     """
+    images = page.images
 
-    page = reader.pages[pagenr]
+    # Check whether only one image is embedded within the page.
+    if len(images) != 1:
+        raise ValueError('Not exactly 1 image present on the page.')
+    else:
+        pdf_image = PdfImage(images[list(images.keys())[0]])
+        pdf_width = float(page.MediaBox[2] - page.MediaBox[0])
+        pdf_height = float(page.MediaBox[3] - page.MediaBox[1])
 
-    xObject = page.Resources.XObject
+        pdf_ratio = pdf_width / pdf_height
+        image_ratio = pdf_image.width / pdf_image.height
 
-    if sum((xObject[obj].Subtype == '/Image')
-            for obj in xObject) != 1:
-        raise ValueError('Not exactly 1 image present on the page')
+        # Check if the aspect ratio of the image is the same as the aspect ratio of the page up to a 3% relative error.
+        if abs(pdf_ratio - image_ratio) > 0.03 * pdf_ratio:
+            raise ValueError('Image has incorrect dimensions')
 
-    for obj in xObject:
-        if xObject[obj].Subtype == '/Image':
-            pdfimage = PdfImage(xObject[obj])
-
-            pdf_width = float(page.MediaBox[2] - page.MediaBox[0])
-            pdf_height = float(page.MediaBox[3] - page.MediaBox[1])
-
-            ratio_width = pdfimage.width / pdf_width
-            ratio_height = pdfimage.height / pdf_height
-
-            # Check if the aspect ratio of the image is the same as the
-            # aspect ratio of the page up to a 3% relative error
-            if abs(ratio_width - ratio_height) > 0.03 * ratio_width:
-                raise ValueError('Image has incorrect dimensions')
-
-            return pdfimage.as_pil_image()
+        return pdf_image.as_pil_image()
 
 
-def extract_image_wand(pagenr, reader, dpi):
-    """Flattens a page from a PDF to an image array
+def extract_image_wand(page, dpi):
+    """Flattens a page from a PDF to an image array.
 
     This method uses Wand to flatten the page and creates an image.
 
     Parameters
     ----------
-    pagenr : int
-        Page number to extract, starting at 0
-    reader : pikepdf.Pdf instance
-        The pdf reader to read the page from
+    page: pikepdf.Page
+        Page to extract into an image
+    dpi:  int
+        The dots per inch of the provided PDF page
 
     Returns
     -------
     img_array : PIL Image
         The extracted image data
     """
-    page = reader.pages[pagenr]
 
     page_pdf = Pdf.new()
     page_pdf.pages.append(page)

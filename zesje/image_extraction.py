84093c4ea851d4e05aef811e379a0ddd9b9c7fc6
Max van Deursen <M.vanDeursen@student.tudelft.nl>
2019-11-10 22:41:29 +0100

    Change start of enumerate

--- zesje/image_extraction.py
+++ zesje/image_extraction.py
@@ -16,7 +16,7 @@ def extract_images(filename, dpi=300):
     with Pdf.open(filename) as pdf_reader:
         use_wand = False
 
-        for page_number, page in enumerate(pdf_reader.pages):
+        for page_number, page in enumerate(pdf_reader.pages, start=1):
             if not use_wand:
                 try:
                     # Try to use PikePDF, but catch any error it raises
@@ -32,7 +32,7 @@ def extract_images(filename, dpi=300):
             if img.mode == 'L':
                 img = img.convert('RGB')
 
-            yield img, page_number + 1
+            yield img, page_number
 
 
 def extract_image_pikepdf(page):

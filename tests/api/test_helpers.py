7b2b5b294a768f059eb1ad45330e4288d985477d
Otto Kaaij <o.k.n.kaaij@student.tudelft.nl>
2019-11-12 12:45:03 +0100

    Change test to reflect _shuffle location change

--- tests/api/test_helpers.py
+++ tests/api/test_helpers.py
@@ -1,4 +1,4 @@
-from zesje.api.submissions import _shuffle_submissions
+from zesje.api._helpers import _shuffle
 
 
 class Submission:
@@ -13,15 +13,15 @@ def test_shuffle_same_grader():
     submissions = [Submission(i) for i in range(5)]
 
     # Assert that shuffled is the same when called twice with the same ID
-    assert _shuffle_submissions(submissions, 0) == _shuffle_submissions(submissions, 0)
+    assert _shuffle(submissions, 0) == _shuffle(submissions, 0)
     # Assert that shuffled is not the same order as unshuffled, but has the same elements
-    assert _shuffle_submissions(submissions, 0) != submissions and \
-        set(submissions) == set(_shuffle_submissions(submissions, 0))
+    assert _shuffle(submissions, 0) != submissions and \
+        set(submissions) == set(_shuffle(submissions, 0))
 
 
 def test_shuffle_different_grader():
     submissions = [Submission(i) for i in range(5)]
     # Assert that shuffled is different when called with different ID's, but has the same elements
-    submissions0 = _shuffle_submissions(submissions, 0)
-    submissions1 = _shuffle_submissions(submissions, 1)
+    submissions0 = _shuffle(submissions, 0)
+    submissions1 = _shuffle(submissions, 1)
     assert submissions0 != submissions1 and set(submissions0) == set(submissions1)

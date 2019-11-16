2125b86b4ad517813f45af08d9d53b5919acb868
Anton Akhmerov <anton.akhmerov@gmail.com>
2019-11-16 18:39:49 +0100

    add issue labeling notebook

---
+++ label_issues.py
@@ -0,0 +1,108 @@
+# # Categorize all issues
+#
+# To use: open with jupyter notebook/lab using jupytext and run all cells
+
+# +
+from getpass import getpass
+from textwrap import dedent
+from collections import defaultdict
+
+from ipywidgets import Button, ToggleButtons, Output, VBox
+from IPython.display import display_markdown, Markdown
+import gitlab
+# -
+
+gl = gitlab.Gitlab(
+    url='https://gitlab.kwant-project.org',
+    private_token=getpass('Gitlab API token: ')
+)
+repo = gl.projects.get('zesje/zesje')
+
+labels = repo.labels.list()
+
+# +
+label_dict = {tuple(l.name.split(': ')): l for l in labels if ': ' in l.name}
+
+categories = ['impact', 'effort', 'maintainability']
+degrees = ['low', 'medium', 'high']
+
+# +
+description = Output()
+
+selector = {
+    category: ToggleButtons(
+        options=[
+            (degree, label_dict[category, degree])
+            for degree in degrees
+        ],
+        description=category,
+        label='medium',
+        style={'button_color': label_dict}
+    )
+    for category in categories
+}
+
+submit = Button(
+    description='submit & next',
+    icon='check'
+)
+
+current_issue = None
+
+
+def submit_labels(issue):
+    other_labels = [i for i in issue.labels if ': ' not in i]
+    issue.labels = [i.value.name for i in selector.values()] + other_labels
+    issue.save()
+
+
+def render_issue(issue):
+    return Markdown(dedent(f"""### [{issue.title}]({issue.web_url})
+
+    {issue.description}
+    """))
+
+
+def next_issue():
+    issues = repo.issues.list(all=True, state='opened')
+    for issue in issues:
+        issue_categories = {
+            l.split(': ')[0]: l.split(': ')[1] for l in issue.labels if ': ' in l
+        }
+        already_labeled = (
+            len(issue_categories) == 3
+            and len(set(issue_categories)) == 3
+        )
+        if not already_labeled:
+            break
+    else:
+        submit.disabled = True
+        submit.text = 'Done!'
+        for button in selector.values():
+            button.disabled = True
+
+        return None
+
+    description.clear_output(wait=True)
+    with description:
+        display(render_issue(issue))
+
+    for category, degree in issue_categories.items():
+        selector[category].label = degree
+
+    return issue
+
+
+def submit_and_next(event):
+    global current_issue
+
+    if current_issue is not None:
+        submit_labels(current_issue)
+
+    current_issue = next_issue()
+
+submit.on_click(submit_and_next)
+
+VBox(
+    children=[description] + list(selector.values()) + [submit]
+)

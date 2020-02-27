Fix units for corner marker detection

zesje/scans.py
@@ -550,7 +550,7 @@ def find_corner_marker_keypoints(image_array, corner_sizes=[0.125, 0.25, 0.5]):
             for label in range(1, ret):
                 new_img = (labels == label)
                 # multiplier determined to work well empirically
-                if np.sum(new_img) > marker_length * marker_width * 5.66:
+                if np.sum(new_img) > marker_length * marker_width * 2.83:
                     continue  # The blob is too large
                 lines = cv2.HoughLines(new_img.astype(np.uint8), 1, np.pi/180, int(marker_length * .9))
                 if lines is None:
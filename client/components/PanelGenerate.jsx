8e20675aed56ae45a7b19ec0a756f8f5439229c2
Hugo Kerstens <hugokk@hotmail.nl>
2020-04-02 10:10:47 +0200

    Remove generate button in exam editor

--- client/components/PanelGenerate.jsx
+++ client/components/PanelGenerate.jsx
@@ -1,9 +1,5 @@
 import React from 'react'
 
-import Notification from 'react-bulma-notification'
-
-import * as api from '../api.jsx'
-
 class PanelGenerate extends React.Component {
   types = [
     'ZIP',
@@ -16,8 +12,6 @@ class PanelGenerate extends React.Component {
     type: this.types[0],
     valid: false,
     editing: true,
-    isGenerating: false,
-    isGenerated: false
   }
 
   pageNumberOrEmpty = new RegExp(/^([1-9]+\d*)?$/)
@@ -46,29 +40,9 @@ class PanelGenerate extends React.Component {
     }
   }
 
-  onClickGenerate = () => {
-    if (this.state.valid) {
-      const postData = {
-        copies_start: this.state.copyRangeStart,
-        copies_end: this.state.copyRangeEnd
-      }
-      this.setState({
-        isGenerating: true
-      }, () => {
-        api.post('exams/' + this.props.examID + '/generated_pdfs', postData)
-          .then(() => {
-            this.setState({
-              isGenerating: false,
-              isGenerated: true
-            })
-          })
-      })
-    } else {
-      Notification.error('Insert proper range')
-    }
-  }
-
   renderInput = (props) => {
+    // Hardcode the width since Bulma does not support the size attribute
+    props.style['width'] = '3.5em'
     return <input
       className={'input ' + this.inputColor()}
       type='text'
@@ -82,8 +56,6 @@ class PanelGenerate extends React.Component {
         if (this.pageNumberOrEmpty.test(e.target.value)) {
           this.setState({
             [props.valueKey]: e.target.value,
-            isGenerated: false,
-            isGenerating: false
           }, () => {
             this.validate()
           })
@@ -102,72 +74,61 @@ class PanelGenerate extends React.Component {
           <label className='label'>Copy number range</label>
         </div>
         <div className='panel-block'>
-          <div className='field has-addons' style={{ maxWidth: '11em' }}>
-            <p className='control'>
-              {this.renderInput({
-                placeholder: 'start',
-                style: { textAlign: 'right' },
-                value: this.state.copyRangeStart,
-                valueKey: 'copyRangeStart'
-              })}
-            </p>
-            <p className='control'>
-              <a className='button is-static'>
-                -
-              </a>
-            </p>
-            <p className='control is-expanded'>
-              {this.renderInput({
-                placeholder: 'end',
-                style: { textAlign: 'left' },
-                value: this.state.copyRangeEnd,
-                valueKey: 'copyRangeEnd'
-              })}
-            </p>
-          </div>
-        </div>
-        <div className='panel-block'>
-          <label className='label'>Generate and download</label>
-        </div>
-        <div className='panel-block'>
-          <div className='field has-addons'>
-            <p className='control'>
-              <span className='select'>
-                <select
-                  onChange={(e) => {
-                    this.setState({
-                      type: e.target.value
-                    })
-                  }}
+          <div className='field is-grouped'>
+            <div className='field has-addons is-marginless'>
+              <p className='control'>
+                {this.renderInput({
+                  placeholder: 'start',
+                  style: { textAlign: 'right' },
+                  value: this.state.copyRangeStart,
+                  valueKey: 'copyRangeStart'
+                })}
+              </p>
+              <p className='control'>
+                <a className='button is-static'>
+                  -
+                </a>
+              </p>
+              <p className='control'>
+                {this.renderInput({
+                  placeholder: 'end',
+                  style: { textAlign: 'left' },
+                  value: this.state.copyRangeEnd,
+                  valueKey: 'copyRangeEnd'
+                })}
+              </p>
+            </div>
+            <div className='field has-addons' style={{ marginLeft: '1em' }}>
+              <p className='control'>
+                <span className='select'>
+                  <select
+                    onChange={(e) => {
+                      this.setState({
+                        type: e.target.value
+                      })
+                    }}
+                  >
+                    {this.types.map((type, index) => {
+                      return <option key={'key_' + index}>{type}</option>
+                    })}
+                  </select>
+                </span>
+              </p>
+              <p className='control'>
+                <a
+                  className={'button is-expanded is-link'}
+                  href={'/api/exams/' + this.props.examID +
+                    '/generated_pdfs' +
+                    '?type=' + this.state.type.toLowerCase() +
+                    '&copies_start=' + parseInt(this.state.copyRangeStart) +
+                    '&copies_end=' + parseInt(this.state.copyRangeEnd)}
                 >
-                  {this.types.map((type, index) => {
-                    return <option key={'key_' + index}>{type}</option>
-                  })}
-                </select>
-              </span>
-            </p>
-            <p className='control'>
-              <button
-                className={'button is-expanded is-link' + (this.state.isGenerating ? ' is-loading' : '')}
-                onClick={this.onClickGenerate}
-                disabled={!this.state.valid || this.state.isGenerated}
-              >
-                Generate
-              </button>
-            </p>
-            <p className='control'>
-              <a
-                className={'button is-expanded is-link'}
-                href={this.state.isGenerated ? ('/api/exams/' + this.props.examID +
-                  '/generated_pdfs' +
-                  '?type=' + this.state.type.toLowerCase() +
-                  '&copies_start=' + parseInt(this.state.copyRangeStart) +
-                  '&copies_end=' + parseInt(this.state.copyRangeEnd)) : null}
-                disabled={!this.state.isGenerated}
-              >
-                Download
-              </a>
-            </p>
+                  <span className='icon is-small'>
+                    <i className='fa fa-download' />
+                  </span>
+                </a>
+              </p>
+            </div>
           </div>
         </div>
       </nav>

38044363e095d1d93370ef87bb79a836626cb191
Hugo Kerstens <hugokk@hotmail.nl>
2020-05-04 16:17:48 +0200

    Remove the need for psutil

--- zesje/mysql.py
+++ zesje/mysql.py
@@ -1,8 +1,8 @@
 import sys
 import os
+import errno
 import configparser
 import time
-import psutil
 import subprocess as sp
 
 
@@ -141,9 +141,39 @@ def _exit(code):
         exit(code)
 
 
+# From https://stackoverflow.com/a/6940314
+def pid_exists(pid):
+    """Check whether pid exists in the current process table.
+    UNIX only.
+    """
+    if pid < 0:
+        return False
+    if pid == 0:
+        # According to "man 2 kill" PID 0 refers to every process
+        # in the process group of the calling process.
+        # On certain systems 0 is a valid PID but we have no way
+        # to know that in a portable fashion.
+        raise ValueError('invalid PID 0')
+    try:
+        os.kill(pid, 0)
+    except OSError as err:
+        if err.errno == errno.ESRCH:
+            # ESRCH == No such process
+            return False
+        elif err.errno == errno.EPERM:
+            # EPERM clearly means there's a process to deny access to
+            return True
+        else:
+            # According to "man 2 kill" possible error values are
+            # (EINVAL, EPERM, ESRCH)
+            raise
+    else:
+        return True
+
+
 def _pid_file(datadir):
     pid_file = Path(datadir) / _defaults_file_option('pid_file')
-    return pid_file if (pid_file.exists() and psutil.pid_exists(int(pid_file.read_text()))) else None
+    return pid_file if (pid_file.exists() and pid_exists(int(pid_file.read_text()))) else None
 
 
 def _create_init_file(filepath, config):

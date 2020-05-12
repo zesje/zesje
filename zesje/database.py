906e6034c0f4cc6c3272a4b0f150d169e1bb6441
Hugo Kerstens <hugokk@hotmail.nl>
2020-05-12 14:47:36 +0200

    Improve feedback_count property

--- zesje/database.py
+++ zesje/database.py
@@ -200,7 +200,7 @@ class Solution(db.Model):
     @property
     def feedback_count(self):
         return object_session(self).query(solution_feedback)\
-            .filter(db.text('solution_id = ' + str(self.id))).count()
+            .filter(solution_feedback.c.solution_id == self.id).count()
 
 
 class Scan(db.Model):

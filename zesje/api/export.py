4d009e36fd50c85c86ce923be8f2caec2e70ee9c
Adria Labay <adria.labay@gmail.com>
2020-05-13 15:06:41 +0200

    export columns in correct order

--- zesje/api/export.py
+++ zesje/api/export.py
@@ -52,22 +52,13 @@ def exam(file_format, exam_id):
 
     try:
         data = full_exam_data(exam_id)
-    except KeyError:
-        abort(404)
+    except KeyError as e:
+        abort(404, message=str(e))
 
     if file_format not in ('dataframe', 'xlsx', 'xlsx_detailed'):
-        abort(404)
+        abort(404, message='File format is not one of [dataframe, xlsx, xlsx_detailed]')
 
     serialized = ResilientBytesIO()
-
-    data.index.name = 'Student ID'
-    data = data.infer_objects()  # convert columns to numeric types if possible
-
-    # move the student names to the first columns
-    cols = data.columns
-    cols_names = data.columns.get_level_values(0).isin(['First name', 'Last name'])
-    cols = list(cols[cols_names]) + list(cols[~cols_names])
-    data = data[cols]
 
     if file_format == 'xlsx':
         cols_total = data.columns.get_level_values(1) == 'total'

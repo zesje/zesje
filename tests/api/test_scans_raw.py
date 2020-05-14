9c1416f717b89a27edfffd05a2a2c64440e4e577
Hugo Kerstens <hugokk@hotmail.nl>
2020-05-14 17:08:12 +0200

    Simplify tests, add test for failed save

--- tests/api/test_scans_raw.py
+++ tests/api/test_scans_raw.py
@@ -2,46 +2,33 @@ import pytest
 import zipfile
 
 from io import BytesIO
-from PIL import Image
 
-from zesje.database import db, Exam, Student
+from zesje.database import db, Exam, Scan
 from zesje.raw_scans import process_zipped_images
 
 
 @pytest.fixture
 def app_with_data(app):
     exam = Exam(name='', finalized=True)
-    students = [Student(id=i+1000000, first_name='', last_name='') for i in range(2)]
     db.session.add(exam)
-    for student in students:
-        db.session.add(student)
     db.session.commit()
-    yield app, exam, students
+    yield app, exam
 
 
 @pytest.fixture
-def image_file():
-    with BytesIO() as image_bytes:
-        image = Image.new('RGB', (10, 10))
-        image.save(image_bytes, format='PNG')
-        yield image_bytes
-
-
-@pytest.fixture
-def zip_file(image_file):
+def zip_file():
     with BytesIO() as zip_bytes:
         with zipfile.ZipFile(zip_bytes, 'w') as z:
-            z.writestr('1000000-1.png', image_file.getvalue())
-            z.writestr('1000001-1.png', image_file.getvalue())
+            z.writestr('1000000-1.png', b'abc')
 
         zip_bytes.seek(0)
         yield zip_bytes
 
 
-def test_no_zip(test_client, app_with_data, image_file):
-    app, exam, students = app_with_data
+def test_no_zip(test_client, app_with_data):
+    app, exam = app_with_data
     data = {
-        'file': (image_file, 'image.jpg')
+        'file': (b'abc', 'image.jpg')
     }
     response = test_client.post(
         f'api/scans/raw/{exam.id}', data=data,
@@ -62,7 +49,7 @@ def test_no_exam(test_client, zip_file):
 
 
 def test_not_finalized_exam(test_client, zip_file, app_with_data):
-    app, exam, students = app_with_data
+    app, exam = app_with_data
     exam.finalized = False
     db.session.commit()
 
@@ -76,8 +63,26 @@ def test_not_finalized_exam(test_client, zip_file, app_with_data):
     assert response.status_code == 403
 
 
+def test_saving_zip_failed(test_client, zip_file, app_with_data, monkeypatch):
+    app, exam = app_with_data
+
+    app_config_mock = app.config.copy()
+    app_config_mock['SCAN_DIRECTORY'] = None
+    monkeypatch.setattr(app, 'config', app_config_mock)
+
+    data = {
+        'file': (zip_file, 'file.zip')
+    }
+    response = test_client.post(
+        f'api/scans/raw/{exam.id}', data=data,
+        content_type='multipart/form-data'
+    )
+    assert response.status_code == 500
+    assert len(Scan.query.all()) == 0
+
+
 def test_processing_started(test_client, zip_file, app_with_data, monkeypatch):
-    app, exam, students = app_with_data
+    app, exam = app_with_data
 
     data = {
         'file': (zip_file, 'file.zip')

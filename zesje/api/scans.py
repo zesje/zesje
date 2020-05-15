6172821858e8da7a97600da872ef684becd68c8f
Hugo Kerstens <hugokk@hotmail.nl>
2020-05-15 21:23:14 +0200

    Merge the normal and scan endpoints

--- zesje/api/scans.py
+++ zesje/api/scans.py
@@ -1,18 +1,20 @@
-import os
-
 from flask import current_app
 from flask_restful import Resource, reqparse
 from werkzeug.datastructures import FileStorage
 
-from ..scans import process_pdf
-from ..raw_scans import process_zipped_images
+from ..scans import process_scan
 from ..database import db, Exam, Scan
-
-ZIP_MIME_TYPES = ['application/zip', 'application/octet-stream', 'application/x-zip-compressed', 'multipart/x-zip']
 
 
 class Scans(Resource):
     """Getting a list of uploaded scans, and uploading new ones."""
+
+    def _is_mimetype_allowed(self, mimetype):
+        return (
+            mimetype == 'application/pdf' or
+            mimetype.startswith('image') or
+            mimetype in current_app.config['ZIP_MIME_TYPES']
+        )
 
     def get(self, exam_id):
         """get all uploaded scans for a particular exam.
@@ -42,8 +44,9 @@ class Scans(Resource):
         ]
 
     post_parser = reqparse.RequestParser()
-    post_parser.add_argument('pdf', type=FileStorage, required=True,
+    post_parser.add_argument('file', type=FileStorage, required=True,
                              location='files')
+    post_parser.add_argument('scan_type', type=str, choices=['normal', 'raw'], required=True)
 
     def post(self, exam_id):
         """Upload a scan PDF
@@ -61,67 +64,8 @@ class Scans(Resource):
         message : str
         """
         args = self.post_parser.parse_args()
-        if args['pdf'].mimetype != 'application/pdf':
-            return dict(message='Uploaded file is not a PDF'), 400
-
-        if (exam := Exam.query.get(exam_id)) is None:
-            return dict(status=404, message='Exam does not exist.'), 404
-        elif not exam.finalized:
-            return dict(status=403, message='Exam is not finalized.'), 403
-
-        scan = Scan(exam=exam, name=args['pdf'].filename,
-                    status='processing', message='Waiting...')
-        db.session.add(scan)
-        db.session.commit()
-
-        try:
-            path = os.path.join(current_app.config['SCAN_DIRECTORY'], f'{scan.id}.pdf')
-            args['pdf'].save(path)
-        except Exception:
-            scan = Scan.query.get(scan.id)
-            if scan is not None:
-                db.session.delete(scan)
-                db.session.commit()
-            raise
-
-        # Fire off a background process
-        # TODO: save these into a process-local datastructure, or save
-        # it into the DB as well so that we can cull 'processing' tasks
-        # that are actually dead.
-        process_pdf.delay(scan_id=scan.id)
-
-        return {
-            'id': scan.id,
-            'name': scan.name,
-            'status': scan.status,
-            'message': scan.message
-        }
-
-
-class RawScans(Resource):
-
-    post_parser = reqparse.RequestParser()
-    post_parser.add_argument('file', type=FileStorage, required=True,
-                             location='files')
-
-    def post(self, exam_id):
-        """Upload a zip with images
-
-        Parameters
-        ----------
-        exam_id : int
-        file : FileStorage
-
-        Returns
-        -------
-        id : int
-        name : str
-        status : str
-        message : str
-        """
-        args = self.post_parser.parse_args()
-        if args['file'].mimetype not in ZIP_MIME_TYPES:
-            return dict(message='Uploaded file is not a ZIP'), 400
+        if not self._is_mimetype_allowed(args['file'].mimetype):
+            return dict(message='File is not a PDF, ZIP or image.'), 400
 
         if (exam := Exam.query.get(exam_id)) is None:
             return dict(status=404, message='Exam does not exist.'), 404
@@ -134,8 +78,7 @@ class RawScans(Resource):
         db.session.commit()
 
         try:
-            path = os.path.join(current_app.config['SCAN_DIRECTORY'], f'{scan.id}.zip')
-            args['file'].save(path)
+            args['file'].save(scan.path)
         except Exception:
             scan = Scan.query.get(scan.id)
             if scan is not None:
@@ -147,7 +90,7 @@ class RawScans(Resource):
         # TODO: save these into a process-local datastructure, or save
         # it into the DB as well so that we can cull 'processing' tasks
         # that are actually dead.
-        process_zipped_images.delay(scan_id=scan.id)
+        process_scan.delay(scan_id=scan.id, scan_type=args['scan_type'])
 
         return {
             'id': scan.id,

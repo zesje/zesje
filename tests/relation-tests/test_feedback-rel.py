56b46d97ea1824df8abc0835b1ce0ce962281ec8
Waded Oudhuis <w.j.oudhuis@student.tudelft.nl>
2021-05-10 15:24:27 +0200

    552 - Write test to create a new feedback option. Restructuring of tests directory is temporary so we can only run the relevant tests (instead of all).

---
+++ tests/relation-tests/test_feedback-rel.py
@@ -0,0 +1,40 @@
+import pytest
+
+from flask import json
+
+from zesje.database import db, Exam, Problem, ProblemWidget
+
+
+@pytest.fixture
+def add_test_data(app):
+    exam1 = Exam(id=1, name='exam 1', finalized=False)
+    db.session.add(exam1)
+
+    problem1 = Problem(id=1, name='Problem 1', exam_id=1)
+    db.session.add(problem1)
+
+    problem_widget_1 = ProblemWidget(id=1, name='problem widget', problem_id=1, page=2,
+                                     width=100, height=150, x=40, y=200, type='problem_widget')
+    db.session.add(problem_widget_1)
+
+    db.session.commit()
+
+
+def fo_json():
+    return {
+        'name': "fully correct",
+        'description': "",
+        'score': 4
+    }
+
+# Actual tests
+
+def test_create__fo(test_client, add_test_data):
+    fo = fo_json()
+
+    result = test_client.post('/api/feedback/1', data=fo)
+    data = json.loads(result.data)
+
+    assert data['name'] == fo['name']
+    assert data['description'] == fo['description']
+    assert data['score'] == fo['score']

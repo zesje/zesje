db32c1f9cbe98ed972d4216dee772473dc1d5450
Scott Jochems <scottiej2001@gmail.com>
2021-05-12 16:17:43 +0200

    Added a test to catch the error of a problem not existing

--- tests/api/test_submissions.py
+++ tests/api/test_submissions.py
@@ -59,3 +59,32 @@ def test_find_next(add_test_data):
 
     result = _find_submission(sub, 20, 1, 'next', False, set([3]), set([2]))
     assert result == sub_to_data(sub2)
+
+
+def test_error_code(add_test_data):
+
+    feedback_option1 = FeedbackOption(id=1, problem_id=20, text='text', description='desc', score=1)
+
+    feedback_option2 = FeedbackOption(id=2, problem_id=20, text='text', description='desc', score=1)
+
+    feedback_option3 = FeedbackOption(id=3, problem_id=20, text='text', description='desc', score=1)
+
+    student = Student(first_name='', last_name='')
+
+    student2 = Student(first_name='bob', last_name='alice')
+
+    grader = Grader(name='Zesje')
+
+    sub = Submission(id=25, student=student, exam_id=42)
+
+    sub2 = Submission(id=26, student=student2, exam_id=42)
+
+    sol = Solution(problem_id=20, submission=sub, graded_by=grader, graded_at=datetime.now(), )
+    db.session.add(sol)
+    sol2 = Solution(problem_id=20, submission=sub2, graded_by=grader, graded_at=datetime.now())
+
+    sol.feedback = [feedback_option1, feedback_option2]
+    sol2.feedback = [feedback_option3]
+
+    result = _find_submission(sub, 12, 1, 'next', False, set([3]), set([2]))
+    assert result[1] == 404

aa18935341dc6f822985d1993c255b3dc02b6472
Nina Immig <N.M.Immig@student.tudelft.nl>
2021-05-20 15:34:34 +0200

    Adding a feedback option without parent works again
    On the different view the feedback option can be added again now.

--- client/components/feedback/FeedbackPanel.jsx
+++ client/components/feedback/FeedbackPanel.jsx
@@ -174,14 +174,8 @@ class FeedbackPanel extends React.Component {
               goBack={this.backToFeedback} showCancel={this.state.feedbackToEditId !== -1} updateFeedback={this.props.updateFeedback} />
         ))}
         {(this.state.feedbackToEditId === -1)
-          ? <div className='panel-block'>
-            <button className='button is-link is-outlined is-fullwidth' onClick={() => this.editFeedback(-1)}>
-              <span className='icon is-small'>
-                <i className='fa fa-plus' />
-              </span>
-              <span>option here</span>
-            </button>
-          </div>
+          ? <FeedbackBlockEdit feedback={null} problemID={this.state.problemID} goBack={this.backToFeedback}
+            updateFeedback={this.props.updateFeedback} />
           : <div className='panel-block'>
             <button className='button is-link is-outlined is-fullwidth' onClick={() => this.editFeedback(-1)}>
               <span className='icon is-small'>

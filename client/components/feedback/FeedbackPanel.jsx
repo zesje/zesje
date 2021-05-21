ba2d4a67e48323c9b6be3b6f85c361ada0a9df30
Nina Immig <N.M.Immig@student.tudelft.nl>
2021-05-21 10:29:33 +0200

    Filter icons also hidden and cancel button doesn't exist in adding view
    As the cancel button currently had the same functionality as the "go back" - button, it wasn't necessary in the additional view. The filter icons, added with the new Merges, are now also hidden.

--- client/components/feedback/FeedbackPanel.jsx
+++ client/components/feedback/FeedbackPanel.jsx
@@ -171,10 +171,10 @@ class FeedbackPanel extends React.Component {
               applyFilter={(e, newFilterMode) => this.props.applyFilter(e, feedback.id, newFilterMode)}
             />
             : <FeedbackBlockEdit key={feedback.id} feedback={feedback} problemID={this.state.problemID}
-              goBack={this.backToFeedback} updateFeedback={this.props.updateFeedback} />
+              goBack={this.backToFeedback} showCancel={this.state.feedbackToEditId !== -1} updateFeedback={this.props.updateFeedback} />
         ))}
         {(this.state.feedbackToEditId === -1)
-          ? <FeedbackBlockEdit feedback={null} problemID={this.state.problemID} goBack={this.backToFeedback}
+          ? <FeedbackBlockEdit feedback={null} problemID={this.state.problemID} goBack={() => this.editFeedback(-1)} showCancel={this.state.feedbackToEditId !== -1}
             updateFeedback={this.props.updateFeedback} />
           : <div className='panel-block'>
             <button className='button is-link is-outlined is-fullwidth' onClick={() => this.editFeedback(-1)}>

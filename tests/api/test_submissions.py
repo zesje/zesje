6ab37e0435f626432f6f2a0365e7a46d7c620720
nathalievandewerken <N.J.A.vandeWerken@student.tudelft.nl>
2021-06-04 15:10:57 +0200

    Added tests to get_submissions.
    Co-authored-by: Alex Maat <A.Maat@student.tudelft.nl>

--- tests/api/test_submissions.py
+++ tests/api/test_submissions.py
@@ -29,6 +29,28 @@ def get_second_feedback():
     return [feedback_option3]
 
 
+@pytest.fixture
+def add_test_submissions(app):
+    student = Student(first_name='Harry', last_name='Lewis')
+    student2 = Student(first_name='bob', last_name='alice')
+    student3 = Student(first_name='Charlie', last_name='Bob')
+
+    grader = Grader(id=1, name='Zesje', oauth_id='Zesje')
+    grader2 = Grader(id=2, name='Alice', oauth_id='Smith')
+
+    sub = Submission(id=25, student=student, exam_id=42)
+    sub2 = Submission(id=26, student=student2, exam_id=42)
+    sub3 = Submission(id=27, student=student3, exam_id=42)
+
+    sol = Solution(problem_id=20, submission=sub, graded_by=grader, graded_at=datetime.now())
+    db.session.add(sol)
+    sol2 = Solution(problem_id=20, submission=sub2, graded_by=grader2, graded_at=datetime.now())
+    db.session.add(sol2)
+    sol3 = Solution(problem_id=20, submission=sub3, graded_by=grader2, graded_at=datetime.now())
+    db.session.add(sol3)
+    yield app
+
+
 def test_has_all_required(get_first_feedback):
     student = Student(first_name='', last_name='')
     grader = Grader(name='Zesje', oauth_id='Zesje')
@@ -104,3 +126,34 @@ def test_find_length(add_test_data, get_first_feedback, get_second_feedback):
 
     result = find_number_of_matches(Problem.query.get(20), False, [1], [], 2)
     assert result == 1
+
+
+def test_get_all_submissions(test_client, add_test_data, add_test_submissions):
+    res = test_client.get('/api/submissions/42')
+    data = res.get_json()
+    assert len(data) == 3
+
+
+def test_exam_not_found(test_client, add_test_data, add_test_submissions):
+    res = test_client.get('/api/submissions/43')
+    assert res.status_code == 404
+
+
+def test_submission_not_found(test_client, add_test_data, add_test_submissions):
+    res = test_client.get('/api/submissions/42/404')
+    assert res.status_code == 404
+
+
+def test_problem_not_found(test_client, add_test_data, add_test_submissions):
+    res = test_client.get('/api/submissions/42/25?problem_id=102')
+    assert res.status_code == 404
+
+
+def test_submission_from_different_exam(test_client, add_test_data, add_test_submissions):
+    exam = Exam(id=43, name='exam g', finalized=True, layout="unstructured")
+    db.session.add(exam)
+    problem = Problem(id=21, name='Problem sadder', exam_id=43)
+    db.session.add(problem)
+
+    res = test_client.get('/api/submissions/43/25')
+    assert res.status_code == 400

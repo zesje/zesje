1bcb17200183874429290a9d13605817e33cc03a
Waded Oudhuis <w.j.oudhuis@student.tudelft.nl>
2021-06-17 10:30:19 +0200

    553 - database changes.

--- migrations/0a9fed7804cd_add_tree_fo.py
+++ migrations/0a9fed7804cd_add_tree_fo.py
@@ -19,8 +19,15 @@ def upgrade():
         batch_op.add_column(sa.Column('parent_id', sa.Integer(), nullable=True))
         batch_op.create_foreign_key(None, 'feedback_option', ['parent_id'], ['id'])
 
+    connection = op.get_bind()
+    rows = connection.execute("SELECT id FROM problem")
+    for row in rows:
+        root = connection.execute(f"INSERT INTO feedback_option (text, score, problem_id) VALUES ('root', 0, {row.id})")
+        connection.execute(f"UPDATE feedback_option SET feedback_option.parent_id = {root.id} WHERE feedback_option.id != {root.id} AND feedback_option.problem_id = {row.id}")
+
 
 def downgrade():
     with op.batch_alter_table('feedback_option', schema=None) as batch_op:
         batch_op.drop_constraint(None, type_='foreignkey')
         batch_op.drop_column('parent_id')
+

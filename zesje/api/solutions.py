ffd77cb008b1d861479cceb4b13503aa4dbc89dc
Anton Akhmerov <anton.akhmerov@gmail.com>
2021-12-01 17:20:42 +0000

    do not change grader when modifying remarks

--- zesje/api/solutions.py
+++ zesje/api/solutions.py
@@ -86,9 +86,6 @@ class Solutions(Resource):
             return dict(status=404, message='Solution does not exist.'), 404
 
         solution.remarks = args.remark
-        if len(solution.feedback):
-            solution.graded_by = current_user
-            solution.graded_at = datetime.now()
 
         db.session.commit()
         return True

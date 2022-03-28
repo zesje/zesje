9b4a26fd6b0d7d801d95e947b4604bd2738959a5
Hugo Kerstens <hugokk@hotmail.nl>
2022-03-28 23:58:20 +0200

    New styling for login screen

--- client/views/Login.jsx
+++ client/views/Login.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import { useLocation, Navigate } from 'react-router-dom'
 
-import zesjeImage from '../components/zesje.png'
-
 const OAuthButton = ({ provider, from }) => (
   provider != null
-    ? <a className='button is-link' href={'/api/oauth/start?userurl=' + from}>
+    ? <a className='button is-info is-inverted' href={'/api/oauth/start?userurl=' + from}>
       <span className='icon'>
         <i className='fa fa-user' aria-hidden='true' />
       </span>
@@ -13,7 +11,7 @@ const OAuthButton = ({ provider, from }) => (
         Login with {provider}
       </span>
     </a>
-    : <p>Could not retrive login provider. Please, try reloading the page.</p>
+    : <p>Could not retrieve login provider. Please, try reloading the page.</p>
 )
 
 const Login = (props) => {
@@ -25,19 +23,11 @@ const Login = (props) => {
   return <section className='hero is-fullheight is-vcentered is-info'>
   <div className="hero-body">
     <div className="container has-text-centered is-flex-grow-0">
-    <p className="title">
-      Welcome to Zesje!
-    </p>
-      <div className='card'>
-        <div className="card-image">
-          <figure className="image is-fullwidth">
-            <img src={zesjeImage.src} alt="Zesje"/>
-          </figure>
-        </div>
-        <div className="card-content">
-          <OAuthButton provider={props.provider} from={from} />
-        </div>
-    </div>
+      <p className="title" style={{ 'font-size': '4rem' }}>
+        <i className='fa fa-edit' />
+        &nbsp;Zesje
+      </p>
+      <OAuthButton provider={props.provider} from={from} />
     </div>
   </div>
   </section>

55913c265004222abc52c23d85ee217c476b9ba5
adria.labay <adria.labay@gmail.com>
2022-07-06 13:18:42 +0200

    increase fail page size

--- client/views/Fail.jsx
+++ client/views/Fail.jsx
@@ -4,9 +4,9 @@ const Home = (props) => {
   return (
     <div className='notification is-danger has-text-centered content'>
 
-      <h1>Oops!</h1>
+      <h1 className='has-text-white'>Oops!</h1>
 
-      <h5>{props.message ? props.message : "Something went wrong :'("}</h5>
+      <h4 className='has-text-white'>{props.message ? props.message : "Something went wrong :'("}</h4>
 
     </div>
   )
